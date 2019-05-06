28811b76bcafbfabf4a7c2e3f00db2a56a87d9e2
Sean Condon <sean@opennetworking.org>
2019-05-06 15:24:23 +0000

    Adding back in Seans overwrittern southbound

--- pkg/southbound/clientManager.go
+++ pkg/southbound/clientManager.go
@@ -20,7 +20,9 @@ import (
 	"crypto/x509"
 	"errors"
 	"fmt"
+	"github.com/opennetworkinglab/onos-config/pkg/southbound/topocache"
 	"io/ioutil"
+	"log"
 	"time"
 
 	"github.com/golang/protobuf/proto"
@@ -29,16 +31,12 @@ import (
 	gpb "github.com/openconfig/gnmi/proto/gnmi"
 )
 
-type Device struct {
-	Addr, Target, Usr, Pwd, CaPath, CertPath, KeyPath string
-	Timeout                                           time.Duration
-}
-
 //Can be extended, for now is ip:port
 type Key struct {
 	Key string
 }
 
+// Target struct for connecting to gNMI
 type Target struct {
 	Destination client.Destination
 	Clt         client.Impl
@@ -59,33 +57,45 @@ type SubscribeOptions struct {
 
 var targets = make(map[Key]Target)
 
-func createDestination(device Device) (*client.Destination, Key) {
+func createDestination(device topocache.Device) (*client.Destination, Key) {
 	d := &client.Destination{TLS: &tls.Config{}}
 	d.Addrs = []string{device.Addr}
 	d.Target = device.Target
 	d.Timeout = time.Duration(device.Timeout * time.Second)
-	if device.CertPath != "" && device.KeyPath != "" {
-		if device.CaPath != "" {
-			certPool := getCertPool(device.CaPath)
-			d.TLS.RootCAs = certPool
-		} else {
-			fmt.Println("Assuming well know CA for certificate signature")
+	if device.CaPath == "" {
+		log.Println("Loading default CA onfca")
+		d.TLS.RootCAs = getCertPoolDefault()
+	} else {
+		d.TLS.RootCAs = getCertPool(device.CaPath)
+	}
+
+	if device.CertPath == "" && device.KeyPath == "" {
+		// Load default Certificates
+		log.Println("Loading default certificates")
+		clientCerts, err := tls.X509KeyPair([]byte(defaultClientCrt), []byte(defaultClientKey))
+		if err != nil {
+			log.Println("Error loading default certs")
 		}
-		// Certificates
+
+		d.TLS.Certificates = []tls.Certificate{clientCerts}
+	} else if device.CertPath != "" && device.KeyPath != "" {
+		// Load certs given for device
 		d.TLS.Certificates = []tls.Certificate{setCertificate(device.CertPath, device.KeyPath)}
+
 	} else if device.Usr != "" && device.Pwd != "" {
 		//TODO implement
 		// cred := &client.Credentials{}
 		// cred.Username = "test"
 		// cred.Password = "testpwd"
 		//d.Credentials = cred
-		//fmt.Println(*cred)
+		//log.Println(*cred)
 	} else {
 		d.TLS = &tls.Config{InsecureSkipVerify: true}
 	}
 	return d, Key{Key: device.Addr}
 }
 
+// GetTarget attempts to get a specific target from the targets cache
 func GetTarget(key Key) (Target, error) {
 	_, ok := targets[key]
 	if ok {
@@ -97,7 +107,7 @@ func GetTarget(key Key) (Target, error) {
 
 //TODO make asyc
 //TODO lock channel to allow one request to device at each time
-func ConnectTarget(device Device) (Target, Key, error) {
+func ConnectTarget(device topocache.Device) (Target, Key, error) {
 	dest, key := createDestination(device)
 	ctx := context.Background()
 	c, err := gclient.New(ctx, *dest)
@@ -116,7 +126,7 @@ func ConnectTarget(device Device) (Target, Key, error) {
 func setCertificate(pathCert string, pathKey string) tls.Certificate {
 	certificate, err := tls.LoadX509KeyPair(pathCert, pathKey)
 	if err != nil {
-		fmt.Println("could not load client key pair: %v", err)
+		log.Println("could not load client key pair", err)
 	}
 	return certificate
 }
@@ -125,14 +135,23 @@ func getCertPool(CaPath string) *x509.CertPool {
 	certPool := x509.NewCertPool()
 	ca, err := ioutil.ReadFile(CaPath)
 	if err != nil {
-		fmt.Println("could not read %q: %s", CaPath, err)
+		log.Println("could not read", CaPath, err)
 	}
 	if ok := certPool.AppendCertsFromPEM(ca); !ok {
-		fmt.Println("failed to append CA certificates")
+		log.Println("failed to append CA certificates")
 	}
 	return certPool
 }
 
+func getCertPoolDefault() *x509.CertPool {
+	certPool := x509.NewCertPool()
+	if ok := certPool.AppendCertsFromPEM([]byte(onfCaCrt)); !ok {
+		log.Println("failed to append CA certificates")
+	}
+	return certPool
+}
+
+// CapabilitiesWithString allows a request for the capabilities by a string - can be empty
 func CapabilitiesWithString(target Target, request string) (*gpb.CapabilityResponse, error) {
 	r := &gpb.CapabilityRequest{}
 	reqProto := &request
@@ -142,6 +161,7 @@ func CapabilitiesWithString(target Target, request string) (*gpb.CapabilityRespo
 	return Capabilities(target, r)
 }
 
+// Capabilities get capabilities according to a formatted request
 func Capabilities(target Target, request *gpb.CapabilityRequest) (*gpb.CapabilityResponse, error) {
 	response, err := target.Clt.(*gclient.Client).Capabilities(target.Ctxt, request)
 	if err != nil {
@@ -150,6 +170,7 @@ func Capabilities(target Target, request *gpb.CapabilityRequest) (*gpb.Capabilit
 	return response, nil
 }
 
+// GetWithString can make a get request according by a string - can be empty
 func GetWithString(target Target, request string) (*gpb.GetResponse, error) {
 	if request == "" {
 		return nil, errors.New("cannot get and empty request")
@@ -162,6 +183,7 @@ func GetWithString(target Target, request string) (*gpb.GetResponse, error) {
 	return Get(target, r)
 }
 
+// Get can make a get request according to a formatted request
 func Get(target Target, request *gpb.GetRequest) (*gpb.GetResponse, error) {
 	response, err := target.Clt.(*gclient.Client).Get(target.Ctxt, request)
 	if err != nil {
@@ -170,6 +192,7 @@ func Get(target Target, request *gpb.GetRequest) (*gpb.GetResponse, error) {
 	return response, nil
 }
 
+// SetWithString can make a set request according by a string
 func SetWithString(target Target, request string) (*gpb.SetResponse, error) {
 	//TODO modify with key that gets target from map
 	if request == "" {
@@ -183,6 +206,7 @@ func SetWithString(target Target, request string) (*gpb.SetResponse, error) {
 	return Set(target, r)
 }
 
+// Set can make a set request according to a formatted request
 func Set(target Target, request *gpb.SetRequest) (*gpb.SetResponse, error) {
 	response, err := target.Clt.(*gclient.Client).Set(target.Ctxt, request)
 	if err != nil {
