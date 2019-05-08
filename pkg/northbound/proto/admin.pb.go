5cc971ab88e065685cb49fb9ed3372785ec7111d
Thomas Vachuska <tom@opennetworking.org>
2019-05-08 20:29:45 +0200

    Added a simple streaming RPC to the admin northbound

--- pkg/northbound/proto/admin.pb.go
+++ pkg/northbound/proto/admin.pb.go
@@ -86,23 +86,67 @@ func (m *HelloWorldResponse) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_HelloWorldResponse proto.InternalMessageInfo
 
+type Line struct {
+	Str                  string   `protobuf:"bytes,1,opt,name=Str,json=str,proto3" json:"Str,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *Line) Reset()         { *m = Line{} }
+func (m *Line) String() string { return proto.CompactTextString(m) }
+func (*Line) ProtoMessage()    {}
+func (*Line) Descriptor() ([]byte, []int) {
+	return fileDescriptor_a91573d9ad3811b0, []int{2}
+}
+
+func (m *Line) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Line.Unmarshal(m, b)
+}
+func (m *Line) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Line.Marshal(b, m, deterministic)
+}
+func (m *Line) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Line.Merge(m, src)
+}
+func (m *Line) XXX_Size() int {
+	return xxx_messageInfo_Line.Size(m)
+}
+func (m *Line) XXX_DiscardUnknown() {
+	xxx_messageInfo_Line.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_Line proto.InternalMessageInfo
+
+func (m *Line) GetStr() string {
+	if m != nil {
+		return m.Str
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*HelloWorldRequest)(nil), "proto.HelloWorldRequest")
 	proto.RegisterType((*HelloWorldResponse)(nil), "proto.HelloWorldResponse")
+	proto.RegisterType((*Line)(nil), "proto.Line")
 }
 
 func init() { proto.RegisterFile("pkg/northbound/proto/admin.proto", fileDescriptor_a91573d9ad3811b0) }
 
 var fileDescriptor_a91573d9ad3811b0 = []byte{
-	// 128 bytes of a gzipped FileDescriptorProto
+	// 177 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x52, 0x28, 0xc8, 0x4e, 0xd7,
 	0xcf, 0xcb, 0x2f, 0x2a, 0xc9, 0x48, 0xca, 0x2f, 0xcd, 0x4b, 0xd1, 0x2f, 0x28, 0xca, 0x2f, 0xc9,
 	0xd7, 0x4f, 0x4c, 0xc9, 0xcd, 0xcc, 0xd3, 0x03, 0xb3, 0x85, 0x58, 0xc1, 0x94, 0x92, 0x30, 0x97,
 	0xa0, 0x47, 0x6a, 0x4e, 0x4e, 0x7e, 0x78, 0x7e, 0x51, 0x4e, 0x4a, 0x50, 0x6a, 0x61, 0x69, 0x6a,
-	0x71, 0x89, 0x92, 0x08, 0x97, 0x10, 0xb2, 0x60, 0x71, 0x41, 0x7e, 0x5e, 0x71, 0xaa, 0x91, 0x3f,
-	0x17, 0x97, 0x1f, 0xdc, 0x44, 0x21, 0x47, 0x2e, 0x8e, 0xe0, 0xc4, 0x4a, 0xb0, 0x32, 0x21, 0x09,
-	0x88, 0x99, 0x7a, 0x18, 0x26, 0x49, 0x49, 0x62, 0x91, 0x81, 0x18, 0xa7, 0xc4, 0x90, 0xc4, 0x06,
-	0x96, 0x33, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0xa1, 0x26, 0xa6, 0x72, 0xad, 0x00, 0x00, 0x00,
+	0x71, 0x89, 0x92, 0x08, 0x97, 0x10, 0xb2, 0x60, 0x71, 0x41, 0x7e, 0x5e, 0x71, 0xaa, 0x92, 0x04,
+	0x17, 0x8b, 0x4f, 0x66, 0x5e, 0xaa, 0x90, 0x00, 0x17, 0x73, 0x70, 0x49, 0x91, 0x04, 0xa3, 0x02,
+	0xa3, 0x06, 0x67, 0x10, 0x73, 0x71, 0x49, 0x91, 0x51, 0x05, 0x17, 0x97, 0x1f, 0xdc, 0x2e, 0x21,
+	0x75, 0x2e, 0xd6, 0xe0, 0x8c, 0xd4, 0x9c, 0x1c, 0x21, 0x6e, 0x88, 0x55, 0x7a, 0x20, 0x5d, 0x52,
+	0xc8, 0x1c, 0x25, 0x06, 0x0d, 0x46, 0x03, 0x46, 0x21, 0x47, 0x2e, 0x8e, 0xe0, 0xc4, 0x4a, 0xb0,
+	0x4d, 0x42, 0x12, 0x50, 0x69, 0x0c, 0xc7, 0x48, 0x49, 0x62, 0x91, 0x81, 0xba, 0x88, 0x21, 0x89,
+	0x0d, 0x2c, 0x67, 0x0c, 0x08, 0x00, 0x00, 0xff, 0xff, 0xda, 0x0d, 0x64, 0x5c, 0xf0, 0x00, 0x00,
+	0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -117,6 +161,7 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type NorthboundClient interface {
+	Shell(ctx context.Context, opts ...grpc.CallOption) (Northbound_ShellClient, error)
 	SayHello(ctx context.Context, in *HelloWorldRequest, opts ...grpc.CallOption) (*HelloWorldResponse, error)
 }
 
@@ -126,6 +171,37 @@ type northboundClient struct {
 
 func NewNorthboundClient(cc *grpc.ClientConn) NorthboundClient {
 	return &northboundClient{cc}
+}
+
+func (c *northboundClient) Shell(ctx context.Context, opts ...grpc.CallOption) (Northbound_ShellClient, error) {
+	stream, err := c.cc.NewStream(ctx, &_Northbound_serviceDesc.Streams[0], "/proto.Northbound/Shell", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &northboundShellClient{stream}
+	return x, nil
+}
+
+type Northbound_ShellClient interface {
+	Send(*Line) error
+	Recv() (*Line, error)
+	grpc.ClientStream
+}
+
+type northboundShellClient struct {
+	grpc.ClientStream
+}
+
+func (x *northboundShellClient) Send(m *Line) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *northboundShellClient) Recv() (*Line, error) {
+	m := new(Line)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
 }
 
 func (c *northboundClient) SayHello(ctx context.Context, in *HelloWorldRequest, opts ...grpc.CallOption) (*HelloWorldResponse, error) {
@@ -139,6 +215,7 @@ func (c *northboundClient) SayHello(ctx context.Context, in *HelloWorldRequest, 
 
 // NorthboundServer is the server API for Northbound service.
 type NorthboundServer interface {
+	Shell(Northbound_ShellServer) error
 	SayHello(context.Context, *HelloWorldRequest) (*HelloWorldResponse, error)
 }
 
@@ -146,12 +223,41 @@ type NorthboundServer interface {
 type UnimplementedNorthboundServer struct {
 }
 
+func (*UnimplementedNorthboundServer) Shell(srv Northbound_ShellServer) error {
+	return status.Errorf(codes.Unimplemented, "method Shell not implemented")
+}
 func (*UnimplementedNorthboundServer) SayHello(ctx context.Context, req *HelloWorldRequest) (*HelloWorldResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method SayHello not implemented")
 }
 
 func RegisterNorthboundServer(s *grpc.Server, srv NorthboundServer) {
 	s.RegisterService(&_Northbound_serviceDesc, srv)
+}
+
+func _Northbound_Shell_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(NorthboundServer).Shell(&northboundShellServer{stream})
+}
+
+type Northbound_ShellServer interface {
+	Send(*Line) error
+	Recv() (*Line, error)
+	grpc.ServerStream
+}
+
+type northboundShellServer struct {
+	grpc.ServerStream
+}
+
+func (x *northboundShellServer) Send(m *Line) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *northboundShellServer) Recv() (*Line, error) {
+	m := new(Line)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
 }
 
 func _Northbound_SayHello_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -181,6 +287,13 @@ var _Northbound_serviceDesc = grpc.ServiceDesc{
 			Handler:    _Northbound_SayHello_Handler,
 		},
 	},
-	Streams:  []grpc.StreamDesc{},
+	Streams: []grpc.StreamDesc{
+		{
+			StreamName:    "Shell",
+			Handler:       _Northbound_Shell_Handler,
+			ServerStreams: true,
+			ClientStreams: true,
+		},
+	},
 	Metadata: "pkg/northbound/proto/admin.proto",
 }
