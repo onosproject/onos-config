f7429988a9c1e690c9ee9e2980b7ca5c89aac50b
Ray Milkey <ray@opennetworking.org>
2019-05-08 16:20:38 +0200

    Add test for path splitting

--- pkg/southbound/utils_test.go
+++ pkg/southbound/utils_test.go
@@ -14,12 +14,23 @@ const (
 	path1 = elemName1 + "[" + elemKeyName1 + "=" + elemKeyValue1 + "]"
 	escapedPath1 = elemNameEscaped1 + "[" + elemKeyName1 + "=" + elemKeyValue1 + "]"
 
-	// /test1:cont1a/list2a=txout1/tx-power
-	elemName2 = "/test/test"
+	// /test1[list2a=txout1]
+	elemName2 = "/test1"
 	elemKeyName2 = "list2a"
 	elemKeyValue2 = "txout1"
-	path2 = "/test/test[list2a=txout1]"
+	path2 = elemName2 + "[" + elemKeyName2 + "=" + elemKeyValue2 + "]"
 
+	pathSegment1 = "1"
+	pathSegment2 = "2[a=b]"
+	pathSegment3 = "3"
+	pathSegment4 = "\\4"
+	pathToSplit = "/" + pathSegment1 + "/" + pathSegment2 + "/" + pathSegment3 + "/" + pathSegment4
+
+	path2Segment1 = "11"
+	path2Segment2 = "22[a=b]"
+	path2Segment3 = "33"
+	path2Segment4 = "\\44"
+	path2ToSplit = "/" + path2Segment1 + "/" + path2Segment2 + "/" + path2Segment3 + "/" + path2Segment4
 )
 
 func checkElement(t *testing.T, parsed *gnmi.Path, index int, elemName string, elemKeyName string, elemKeyValue string) {
@@ -81,4 +92,26 @@ func Test_ParseMultiple(t *testing.T) {
 
 	checkElement(t, parsed, 0,  elemName1, elemKeyName1, elemKeyValue1)
 	checkElement(t, parsed, 1,  elemName2, elemKeyName2, elemKeyValue2)
+}
+
+func Test_Split(t *testing.T) {
+	paths := make([]string, 2)
+	paths[0] = pathToSplit
+	paths[1] = path2ToSplit
+	splitPaths := SplitPaths(paths)
+	if len(splitPaths) != 2 {
+		t.Errorf("paths split length is incorrect %d", len(splitPaths))
+		return
+	}
+
+	splitPaths1 := splitPaths[0]
+	if splitPaths1[0] != pathSegment1 {
+		t.Errorf("paths split segment 1 is incorrect %s", splitPaths[0])
+		return
+	}
+
+	if splitPaths1[3] != pathSegment4 {
+		t.Errorf("paths split segment 4 is incorrect %s", splitPaths[3])
+		return
+	}
 }
