3d14509fe51d761d002744f143c97328c0a0ba10
Ray Milkey <ray@opennetworking.org>
2019-05-08 17:13:59 +0200

    Utils test (#61)
    * Add test for path splitting
    
    * Added license header
    
    * Add checks for syntax errors

--- pkg/southbound/utils_test.go
+++ pkg/southbound/utils_test.go
@@ -1,3 +1,17 @@
+// Copyright 2019-present Open Networking Foundation
+//
+// Licensed under the Apache License, Configuration 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package southbound
 
 import (
@@ -14,12 +28,26 @@ const (
 	path1 = elemName1 + "[" + elemKeyName1 + "=" + elemKeyValue1 + "]"
 	escapedPath1 = elemNameEscaped1 + "[" + elemKeyName1 + "=" + elemKeyValue1 + "]"
 
-	// /test1:cont1a/list2a=txout1/tx-power
-	elemName2 = "/test/test"
+	// /test1[list2a=txout1]
+	elemName2 = "/test1"
 	elemKeyName2 = "list2a"
 	elemKeyValue2 = "txout1"
-	path2 = "/test/test[list2a=txout1]"
+	path2 = elemName2 + "[" + elemKeyName2 + "=" + elemKeyValue2 + "]"
 
+	pathSegment1 = "1"
+	pathSegment2 = "2[a=b]"
+	pathSegment3 = "3"
+	pathSegment4 = "\\4"
+	pathToSplit = "/" + pathSegment1 + "/" + pathSegment2 + "/" + pathSegment3 + "/" + pathSegment4
+
+	path2Segment1 = "11"
+	path2Segment2 = "22[a=b]"
+	path2Segment3 = "33"
+	path2Segment4 = "\\44"
+	path2ToSplit = "/" + path2Segment1 + "/" + path2Segment2 + "/" + path2Segment3 + "/" + path2Segment4
+
+	pathNoClose = "/a/b[name=aaa"
+	pathNoKeyName = "/a/b/[=aaa]"
 )
 
 func checkElement(t *testing.T, parsed *gnmi.Path, index int, elemName string, elemKeyName string, elemKeyValue string) {
@@ -81,4 +109,48 @@ func Test_ParseMultiple(t *testing.T) {
 
 	checkElement(t, parsed, 0,  elemName1, elemKeyName1, elemKeyValue1)
 	checkElement(t, parsed, 1,  elemName2, elemKeyName2, elemKeyValue2)
+}
+
+func Test_ParseErrorNoClose(t *testing.T) {
+	elements := make([]string, 1)
+	elements[0] = pathNoClose
+	parsed, err := ParseGNMIElements(elements)
+
+	if err == nil || parsed != nil {
+		t.Errorf("path with no closing bracket did not generate an error")
+		return
+	}
+}
+
+func Test_ParseErrorNoKeyName(t *testing.T) {
+	elements := make([]string, 1)
+	elements[0] = pathNoKeyName
+	parsed, err := ParseGNMIElements(elements)
+
+	if err == nil || parsed != nil {
+		t.Errorf("path with no opening bracket did not generate an error")
+		return
+	}
+}
+
+func Test_Split(t *testing.T) {
+	paths := make([]string, 2)
+	paths[0] = pathToSplit
+	paths[1] = path2ToSplit
+	splitPaths := SplitPaths(paths)
+	if len(splitPaths) != 2 {
+		t.Errorf("paths split length is incorrect %d", len(splitPaths))
+		return
+	}
+
+	splitPaths1 := splitPaths[0]
+	if splitPaths1[0] != pathSegment1 {
+		t.Errorf("paths split segment 1 is incorrect %s", splitPaths[0])
+		return
+	}
+
+	if splitPaths1[3] != pathSegment4 {
+		t.Errorf("paths split segment 4 is incorrect %s", splitPaths[3])
+		return
+	}
 }
