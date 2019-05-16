62bf38e5560dc52eecb020e3e1236e260ded0293
adibrastegarnia <arastega@purdue.edu>
2019-05-16 17:59:03 -0400

    Pass a context explicitly to each function - use receiver type functions for gnmi southbound

--- cmd/southbound_main/southbound_main.go
+++ cmd/southbound_main/southbound_main.go
@@ -15,11 +15,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/onosproject/onos-config/pkg/southbound"
-	"github.com/onosproject/onos-config/pkg/southbound/topocache"
 	"reflect"
 	"time"
+
+	"github.com/onosproject/onos-config/pkg/southbound"
+	"github.com/onosproject/onos-config/pkg/southbound/topocache"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/openconfig/gnmi/client"
@@ -27,35 +29,35 @@ import (
 
 func main() {
 	device := topocache.Device{
-		Addr:     "localhost:10161",
-		Target:   "Test-onos-config",
+		Addr:   "localhost:10161",
+		Target: "Test-onos-config",
 		// Loaded from default-certificates.go
 		//CaPath:   "/Users/andrea/go/src/github.com/opennetworkinglab/onos-config/tools/test/devicesim/certs/onfca.crt",
 		//CertPath: "/Users/andrea/go/src/github.com/opennetworkinglab/onos-config/tools/test/devicesim/certs/client1.crt",
 		//KeyPath:  "/Users/andrea/go/src/github.com/opennetworkinglab/onos-config/tools/test/devicesim/certs/client1.key",
-		Timeout:  time.Second * 10,
+		Timeout: time.Second * 10,
 	}
-	target, err := southbound.GetTarget(southbound.Key{Key: device.Addr})
+	ctx := context.Background()
+	target := southbound.Target{}
+
+	_, err := target.ConnectTarget(ctx, device)
 	if err != nil {
-		fmt.Println("Creating device for addr: ", device.Addr)
-		target, _, err = southbound.ConnectTarget(device)
-		if err != nil {
-			fmt.Println("Error ", target, err)
-		}
+		fmt.Println("Error ", target, err)
 	}
+
 	targetExists, err := southbound.GetTarget(southbound.Key{Key: device.Addr})
 	if reflect.DeepEqual(target, targetExists) {
 		fmt.Println("Target reusal works")
 	}
 	request := ""
-	capResponse, capErr := southbound.CapabilitiesWithString(target, request)
+	capResponse, capErr := target.CapabilitiesWithString(ctx, request)
 	if capErr != nil {
 		fmt.Println("Error ", target, err)
 	}
 	capResponseString := proto.MarshalTextString(capResponse)
 	fmt.Println("Capabilities: ", capResponseString)
 	request = "path: <elem: <name: 'system'> elem:<name:'config'> elem: <name: 'hostname'>>"
-	getResponse, getErr := southbound.GetWithString(target, request)
+	getResponse, getErr := target.GetWithString(ctx, request)
 	if getErr != nil {
 		fmt.Println("Error ", target, err)
 	}
@@ -89,5 +91,5 @@ func main() {
 		}
 		return nil
 	}
-	southbound.Subscribe(target, req, handler)
+	target.Subscribe(ctx, req, handler)
 }
---
+++ pkg/southbound/defs.go
@@ -0,0 +1,76 @@
+// Copyright 2019-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package southbound
+
+import (
+	"github.com/openconfig/gnmi/client"
+)
+
+// Key Target key; can be extended, for now is ip:port
+type Key struct {
+	Key string
+}
+
+// Target struct for connecting to gNMI
+type Target struct {
+	Destination client.Destination
+	Clt         client.Impl
+}
+
+// SubscribeOptions is the gNMI subscription request options
+type SubscribeOptions struct {
+	UpdatesOnly       bool
+	Prefix            string
+	Mode              string
+	StreamMode        string
+	SampleInterval    uint64
+	HeartbeatInterval uint64
+	Paths             [][]string
+	Origin            string
+}
+
+// SubscribeMode specifies the mode of a subscription request
+type SubscribeMode int
+
+// StreamMode specifies the mode of a streaming request
+type StreamMode int
+
+const (
+	// Once mode
+	Once SubscribeMode = iota
+	// Poll mode
+	Poll
+	// Stream mode
+	Stream
+)
+
+const (
+	// OnChange stream mode
+	OnChange StreamMode = iota
+	// Sample stream mode
+	Sample
+	// TargetDefined stream mode
+	TargetDefined
+)
+
+var targets = make(map[Key]interface{})
+
+func (mode SubscribeMode) String() string {
+	return [...]string{"Once", "Poll", "Stream"}[mode]
+}
+
+func (mode StreamMode) String() string {
+	return [...]string{"on_change", "sample", "target_defined"}[mode]
+}
