d71cb79b06a90dadfb504c642155ed3c4122cb8d
Sean Condon <sean@opennetworking.org>
2019-07-08 08:27:36 +0100

    Typed values - added conversion functions

--- pkg/store/change/typedvalue_test.go
+++ pkg/store/change/typedvalue_test.go
@@ -15,59 +15,92 @@
 package change
 
 import (
+	"encoding/base64"
 	"fmt"
 	"gotest.tools/assert"
 	"testing"
 )
 
+const testString = "This is a test"
+const (
+	testNegativeInt = -9223372036854775808
+	testZeroInt     = 0
+	testPositiveInt = 9223372036854775807
+)
+const (
+	testZeroUint   = uint(0)
+	testElevenUint = uint(11)
+	testMaxUint    = uint(18446744073709551615)
+)
+const (
+	testPrecision3 = 3
+	testPrecision6 = 6
+	testDigitsZero = 0
+	testPrecision0 = 0
+)
+const (
+	testFloatNeg = float32(-3.4E+38)
+	testFloatPos = float32(3.4E+38)
+)
+const (
+	testStringBytes    = "onos rocks!"
+	testStringBytesB64 = "b25vcyByb2NrcyE="
+)
+
+var testLeafListString = []string{"abc", "def", "ghi", "with,comma"}
+
+var testLeafListInt = []int{testNegativeInt, testZeroInt, testPositiveInt}
+
+var testLeafListUint = []uint{testZeroUint, testElevenUint, testMaxUint}
+
+var testLeafListBool = []bool{true, false, false, true}
+
+var testLeafListDecimal = []int64{testNegativeInt, testZeroInt, testPositiveInt}
+
+var testLeafListFloat = []float32{testFloatNeg, testZeroInt, testFloatPos}
+
+var testByteArr0 = []byte("abc")
+var testByteArr1 = []byte("defg")
+var testByteArr2 = []byte("ghijk")
+var testLeafListBytes = [][]byte{testByteArr0, testByteArr1, testByteArr2}
+
 func Test_TypedValueEmpty(t *testing.T) {
-	tv := CreateTypedValueEmpty()
+	tv := (CreateTypedValueEmpty())
 
 	assert.Equal(t, len(tv.Value), 0)
-	assert.Equal(t, tv.Type, TypeEmpty)
-	assert.Equal(t, tv.LeafList, false)
+	testConversion(t, (*TypedValue)(tv))
 }
 
 func Test_TypedValueString(t *testing.T) {
-	const testString = "This is a test"
-	tv := CreateTypedValueString(testString)
+	tv := (*TypedValue)(CreateTypedValueString(testString))
 
 	assert.Equal(t, len(tv.Value), 14)
-	assert.Equal(t, tv.String(), testString)
-	assert.Equal(t, tv.Type, TypeString)
-	assert.Equal(t, tv.LeafList, false)
+	testConversion(t, (*TypedValue)(tv))
 }
 
 func Test_TypedValueInt(t *testing.T) {
-	const testNegative = -9223372036854775808
-	tv := CreateTypedValueInt64(testNegative)
-
+	tv := CreateTypedValueInt64(testNegativeInt)
+	assert.Equal(t, tv.String(), fmt.Sprintf("%d", testNegativeInt))
 	assert.Equal(t, len(tv.Value), 8)
-	assert.Equal(t, tv.String(), fmt.Sprintf("%d", testNegative))
-	assert.Equal(t, tv.Type, TypeInt)
-	assert.Equal(t, tv.LeafList, false)
 
-	const testPositive = 9223372036854775807
-	tv = CreateTypedValueInt64(testPositive)
-	assert.Equal(t, tv.String(), fmt.Sprintf("%d", testPositive))
+	testConversion(t, (*TypedValue)(tv))
 
-	const testZero = 0
-	tv = CreateTypedValueInt64(testZero)
-	assert.Equal(t, tv.String(), fmt.Sprintf("%d", testZero))
+	tv = CreateTypedValueInt64(testPositiveInt)
+	assert.Equal(t, tv.String(), fmt.Sprintf("%d", testPositiveInt))
+
+	tv = CreateTypedValueInt64(testZeroInt)
+	assert.Equal(t, tv.String(), fmt.Sprintf("%d", testZeroInt))
 }
 
 func Test_TypedValueUint(t *testing.T) {
-	const testZero = uint(0)
-	tv := CreateTypedValueUint64(testZero)
-
+	tv := CreateTypedValueUint64(testZeroUint)
+	assert.Equal(t, tv.String(), fmt.Sprintf("%d", testZeroUint))
 	assert.Equal(t, len(tv.Value), 8)
-	assert.Equal(t, tv.String(), fmt.Sprintf("%d", testZero))
-	assert.Equal(t, tv.Type, TypeUint)
-	assert.Equal(t, tv.LeafList, false)
 
-	const testMax = uint(18446744073709551615)
-	tv = CreateTypedValueUint64(testMax)
-	assert.Equal(t, tv.String(), fmt.Sprintf("%d", testMax))
+	tv = CreateTypedValueUint64(testMaxUint)
+	assert.Equal(t, tv.String(), fmt.Sprintf("%d", testMaxUint))
+
+	testConversion(t, (*TypedValue)(tv))
 }
 
 func Test_TypedValueBool(t *testing.T) {
@@ -75,134 +108,164 @@ func Test_TypedValueBool(t *testing.T) {
 
 	assert.Equal(t, len(tv.Value), 1)
 	assert.Equal(t, tv.String(), "true")
-	assert.Equal(t, tv.Type, TypeBool)
-	assert.Equal(t, tv.LeafList, false)
+
+	testConversion(t, (*TypedValue)(tv))
 
 	tv = CreateTypedValueBool(false)
 	assert.Equal(t, tv.String(), "false")
 }
 
 func Test_TypedValueDecimal64(t *testing.T) {
-	const testDigitsNeg = -9223372036854775808
-	const testPrecision3 = 3
-	tv := CreateTypedValueDecimal64(testDigitsNeg, testPrecision3)
-
+	tv := CreateTypedValueDecimal64(testNegativeInt, testPrecision3)
 	assert.Equal(t, len(tv.Value), 8)
 	assert.Equal(t, tv.String(), "-9223372036854775.808")
-	assert.Equal(t, tv.Type, fmt.Sprintf("%s-%d", TypeDecimal64, testPrecision3))
-	assert.Equal(t, tv.LeafList, false)
+	assert.Equal(t, tv.TypeOpts[0], testPrecision3)
 
-	const testDigitsPos = 9223372036854775807
-	const testPrecision6 = 6
-	tv = CreateTypedValueDecimal64(testDigitsPos, testPrecision6)
+	testConversion(t, (*TypedValue)(tv))
 
+	tv = CreateTypedValueDecimal64(testPositiveInt, testPrecision6)
 	assert.Equal(t, tv.String(), "9223372036854.775807")
-	assert.Equal(t, tv.Type, fmt.Sprintf("%s-%d", TypeDecimal64, testPrecision6))
+	assert.Equal(t, tv.TypeOpts[0], testPrecision6)
 
-	const testDigitsZero = 0
-	const testPrecision0 = 0
 	tv = CreateTypedValueDecimal64(testDigitsZero, testPrecision0)
-
 	assert.Equal(t, tv.String(), "0.0")
-	assert.Equal(t, tv.Type, fmt.Sprintf("%s-%d", TypeDecimal64, testPrecision0))
+	assert.Equal(t, tv.TypeOpts[0], testPrecision0)
 }
 
 func Test_TypedValueFloat(t *testing.T) {
-	const testFloatNeg = float32(-3.4E+38)
 	tv := CreateTypedValueFloat(testFloatNeg)
-
 	assert.Equal(t, len(tv.Value), 8)
 	assert.Equal(t, tv.String(), "-339999995214436424907732413799364296704.000000")
-	assert.Equal(t, tv.Type, TypeFloat)
-	assert.Equal(t, tv.LeafList, false)
 
-	const testFloatPos = float32(3.4E+38)
+	testConversion(t, (*TypedValue)(tv))
+
 	tv = CreateTypedValueFloat(testFloatPos)
 	assert.Equal(t, tv.String(), "339999995214436424907732413799364296704.000000")
 }
 
 func Test_TypedValueBytes(t *testing.T) {
-	const testString = "onos rocks!"
-	tv := CreateTypedValueBytes([]byte(testString))
-
+	tv := CreateTypedValueBytes([]byte(testStringBytes))
 	assert.Equal(t, len(tv.Value), 11)
-	assert.Equal(t, tv.String(), "b25vcyByb2NrcyE=")
-	assert.Equal(t, tv.Type, TypeBytes)
-	assert.Equal(t, tv.LeafList, false)
+	assert.Equal(t, tv.String(), testStringBytesB64)
+
+	testConversion(t, (*TypedValue)(tv))
 }
 
 func Test_LeafListString(t *testing.T) {
-	testArray := []string{"abc", "def", "ghi", "with,comma"}
-	tv := CreateLeafListString(testArray)
-
+	tv := CreateLeafListString(testLeafListString)
 	assert.Equal(t, len(tv.Value), 22)
 	assert.Equal(t, tv.String(), "abc,def,ghi,with,comma")
-	assert.Equal(t, tv.Type, TypeString)
-	assert.Equal(t, tv.LeafList, true)
+	testConversion(t, (*TypedValue)(tv))
 
-	testArray = []string{"one"}
+	testArray := []string{"one"}
 	tv = CreateLeafListString(testArray)
 	assert.Equal(t, len(tv.Value), 3)
 	assert.Equal(t, tv.String(), "one")
 }
 
 func Test_LeafListInt64(t *testing.T) {
-	testArray := []int{-9223372036854775808, 11, 9223372036854775807}
-	tv := CreateLeafListInt64(testArray)
-
+	tv := CreateLeafListInt64(testLeafListInt)
 	assert.Equal(t, len(tv.Value), 24)
-	assert.Equal(t, tv.String(), "[-9223372036854775808 11 9223372036854775807]")
-	assert.Equal(t, tv.Type, TypeInt)
-	assert.Equal(t, tv.LeafList, true)
+	assert.Equal(t, tv.String(), "[-9223372036854775808 0 9223372036854775807]")
+
+	testConversion(t, (*TypedValue)(tv))
 }
 
 func Test_LeafListUint64(t *testing.T) {
-	testArray := []uint{0, 11, 18446744073709551615}
-	tv := CreateLeafListUint64(testArray)
+	tv := CreateLeafListUint64(testLeafListUint)
 
 	assert.Equal(t, len(tv.Value), 24)
 	assert.Equal(t, tv.String(), "[0 11 18446744073709551615]")
-	assert.Equal(t, tv.Type, TypeUint)
-	assert.Equal(t, tv.LeafList, true)
+
+	testConversion(t, (*TypedValue)(tv))
 }
 
 func Test_LeafListBool(t *testing.T) {
-	testArray := []bool{true, false, false, true}
-	tv := CreateLeafListBool(testArray)
+	tv := CreateLeafListBool(testLeafListBool)
 
 	assert.Equal(t, len(tv.Value), 4)
 	assert.Equal(t, tv.String(), "[true false false true]")
-	assert.Equal(t, tv.Type, TypeBool)
-	assert.Equal(t, tv.LeafList, true)
+
+	testConversion(t, (*TypedValue)(tv))
 }
 
 func Test_LeafListDecimal64(t *testing.T) {
-	const precision = 6
-	testArray := []int64{-9223372036854775808, 11, 9223372036854775807}
-	tv := CreateLeafListDecimal64(testArray, precision)
+	tv := CreateLeafListDecimal64(testLeafListDecimal, testPrecision6)
 
 	assert.Equal(t, len(tv.Value), 24)
-	assert.Equal(t, tv.String(), "[-9223372036854775808 11 9223372036854775807] 6")
-	assert.Equal(t, tv.Type, TypeDecimal64+"-6")
-	assert.Equal(t, tv.LeafList, true)
+	assert.Equal(t, tv.String(), "[-9223372036854775808 0 9223372036854775807] 6")
+
+	testConversion(t, (*TypedValue)(tv))
 }
 
 func Test_LeafListFloat32(t *testing.T) {
-	testArray := []float32{-3.4E+30, 0, 3.4E+30}
-	tv := CreateLeafListFloat32(testArray)
+	tv := CreateLeafListFloat32(testLeafListFloat)
 
 	assert.Equal(t, len(tv.Value), 24)
-	assert.Equal(t, tv.String(), "-3399999900045657695752095268864.000000,0.000000,3399999900045657695752095268864.000000")
-	assert.Equal(t, tv.Type, TypeFloat)
-	assert.Equal(t, tv.LeafList, true)
+	assert.Equal(t, tv.String(), "-339999995214436424907732413799364296704.000000,0.000000,339999995214436424907732413799364296704.000000")
+
+	testConversion(t, (*TypedValue)(tv))
 }
 
 func Test_LeafListBytes(t *testing.T) {
-	testArray := [][]byte{[]byte("abc"), []byte("defg"), []byte("ghijk")}
-	tv := CreateLeafListBytes(testArray)
+	tv := CreateLeafListBytes(testLeafListBytes)
+
+	testConversion(t, (*TypedValue)(tv))
 
 	assert.Equal(t, len(tv.Value), 12)
 	assert.Equal(t, tv.String(), "[[97 98 99] [100 101 102 103] [103 104 105 106 107]]")
-	assert.Equal(t, tv.Type, TypeBytes+"_3_4_5")
-	assert.Equal(t, tv.LeafList, true)
+	assert.Equal(t, tv.TypeOpts[0], 3)
+	assert.Equal(t, tv.TypeOpts[1], 4)
+	assert.Equal(t, tv.TypeOpts[2], 5)
+}
+
+func testConversion(t *testing.T, tv *TypedValue) {
+
+	switch v := tv.Type.(type) {
+	case *TypedEmpty:
+		assert.Equal(t, (*TypedEmpty)(tv).String(), "")
+	case *TypedString:
+		assert.Equal(t, (*TypedString)(tv).String(), testString)
+	case *TypedInt64:
+		assert.Equal(t, (*TypedInt64)(tv).Int(), testNegativeInt)
+	case *TypedUint64:
+		assert.Equal(t, (*TypedUint64)(tv).Uint(), testMaxUint)
+	case *TypedBool:
+		assert.Equal(t, (*TypedBool)(tv).Bool(), true)
+	case *TypedDecimal64:
+		digits, precision := (*TypedDecimal64)(tv).Decimal64()
+		assert.Equal(t, digits, int64(testNegativeInt))
+		assert.Equal(t, precision, uint32(testPrecision3))
+	case *TypedFloat:
+		assert.Equal(t, (*TypedFloat)(tv).Float32(), testFloatNeg)
+	case *TypedBytes:
+		assert.Equal(t, base64.StdEncoding.EncodeToString((*TypedBytes)(tv).Bytes()), testStringBytesB64)
+		assert.Equal(t, len(tv.TypeOpts), 1)
+		assert.Equal(t, tv.TypeOpts[0], 11)
+	case *TypedLeafListString:
+		assert.DeepEqual(t, (*TypedLeafListString)(tv).List(), testLeafListString)
+	case *TypedLeafListInt64:
+		assert.DeepEqual(t, (*TypedLeafListInt64)(tv).List(), testLeafListInt)
+	case *TypedLeafListUint:
+		assert.DeepEqual(t, (*TypedLeafListUint)(tv).List(), testLeafListUint)
+	case *TypedLeafListBool:
+		assert.DeepEqual(t, (*TypedLeafListBool)(tv).List(), testLeafListBool)
+	case *TypedLeafListDecimal:
+		digits, precision := (*TypedLeafListDecimal)(tv).List()
+		assert.DeepEqual(t, digits, testLeafListDecimal)
+		assert.Equal(t, precision, uint32(testPrecision6))
+	case *TypedLeafListFloat:
+		assert.DeepEqual(t, (*TypedLeafListFloat)(tv).List(), testLeafListFloat)
+	case *TypedLeafListBytes:
+		assert.DeepEqual(t, (*TypedLeafListBytes)(tv).List(), testLeafListBytes)
+		assert.Equal(t, len(tv.TypeOpts), 3)
+		assert.Equal(t, tv.TypeOpts[0], 3)
+		assert.Equal(t, tv.TypeOpts[1], 4)
+		assert.Equal(t, tv.TypeOpts[2], 5)
+
+	default:
+		t.Log("Unexpected type", v)
+		t.Fail()
+	}
+
 }
---
+++ pkg/utils/values/gnmiValueUtil_test.go
@@ -0,0 +1,120 @@
+// Copyright 2019-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package utils test various gNMI value manipulation facilities.
+
+package values
+
+import (
+	"github.com/onosproject/onos-config/pkg/store/change"
+	pb "github.com/openconfig/gnmi/proto/gnmi"
+	"gotest.tools/assert"
+	"testing"
+)
+
+const testString = "This is a test"
+const (
+	testNegativeInt = -9223372036854775808
+	testZeroInt     = 0
+	testPositiveInt = 9223372036854775807
+)
+const (
+	testZeroUint   = uint(0)
+	testElevenUint = uint(11)
+	testMaxUint    = uint(18446744073709551615)
+)
+
+////////////////////////////////////////////////////////////////////////////////
+// gNMI format to Native
+////////////////////////////////////////////////////////////////////////////////
+
+func Test_GnmiStringToNative(t *testing.T) {
+	gnmiValue := pb.TypedValue_StringVal{StringVal: testString}
+	nativeType, err := GnmiTypedValueToNativeType(&pb.TypedValue{Value: &gnmiValue})
+	assert.NilError(t, err)
+
+	nativeString := (*change.TypedString)(nativeType)
+	assert.Equal(t, nativeString.String(), testString)
+}
+
+func Test_GnmiIntToNative(t *testing.T) {
+	gnmiValue := pb.TypedValue_IntVal{IntVal: testNegativeInt}
+	nativeType, err := GnmiTypedValueToNativeType(&pb.TypedValue{Value: &gnmiValue})
+	assert.NilError(t, err)
+
+	nativeInt64 := (*change.TypedInt64)(nativeType)
+	assert.Equal(t, nativeInt64.Int(), testNegativeInt)
+}
+
+func Test_GnmiUintToNative(t *testing.T) {
+	gnmiValue := pb.TypedValue_UintVal{UintVal: uint64(testMaxUint)}
+	nativeType, err := GnmiTypedValueToNativeType(&pb.TypedValue{Value: &gnmiValue})
+	assert.NilError(t, err)
+
+	nativeUint64 := (*change.TypedUint64)(nativeType)
+	assert.Equal(t, nativeUint64.Uint(), testMaxUint)
+}
+
+func Test_GnmiBoolToNative(t *testing.T) {
+	gnmiValue := pb.TypedValue_BoolVal{BoolVal: true}
+	nativeType, err := GnmiTypedValueToNativeType(&pb.TypedValue{Value: &gnmiValue})
+	assert.NilError(t, err)
+
+	nativeBool := (*change.TypedBool)(nativeType)
+	assert.Equal(t, nativeBool.Bool(), true)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Native format to gnmi
+////////////////////////////////////////////////////////////////////////////////
+
+func Test_NativeStringToGnmi(t *testing.T) {
+	nativeString := (*change.TypedValue)(change.CreateTypedValueString(testString))
+	gnmiString, err := NativeTypeToGnmiTypedValue(nativeString)
+	assert.NilError(t, err)
+	_, ok := gnmiString.Value.(*pb.TypedValue_StringVal)
+	assert.Assert(t, ok)
+
+	assert.Equal(t, gnmiString.GetStringVal(), testString)
+}
+
+func Test_NativeIntToGnmi(t *testing.T) {
+	nativeInt := (*change.TypedValue)(change.CreateTypedValueInt64(testPositiveInt))
+	gnmiInt, err := NativeTypeToGnmiTypedValue(nativeInt)
+	assert.NilError(t, err)
+	_, ok := gnmiInt.Value.(*pb.TypedValue_IntVal)
+	assert.Assert(t, ok)
+
+	assert.Equal(t, int(gnmiInt.GetIntVal()), testPositiveInt)
+}
+
+func Test_NativeUintToGnmi(t *testing.T) {
+	nativeUint := (*change.TypedValue)(change.CreateTypedValueUint64(testMaxUint))
+	gnmiUint, err := NativeTypeToGnmiTypedValue(nativeUint)
+	assert.NilError(t, err)
+	_, ok := gnmiUint.Value.(*pb.TypedValue_UintVal)
+	assert.Assert(t, ok)
+
+	assert.Equal(t, uint(gnmiUint.GetUintVal()), testMaxUint)
+}
+
+func Test_NativeBoolToGnmi(t *testing.T) {
+	nativeBool := (*change.TypedValue)(change.CreateTypedValueBool(true))
+	gnmiBool, err := NativeTypeToGnmiTypedValue(nativeBool)
+	assert.NilError(t, err)
+	_, ok := gnmiBool.Value.(*pb.TypedValue_BoolVal)
+	assert.Assert(t, ok)
+
+	assert.Equal(t, gnmiBool.GetBoolVal(), true)
+}
