61d5251afa4a5fd650e627d9f5627e67381f1163
Ray Milkey <ray@opennetworking.org>
2019-07-09 06:44:00 -0700

    Back to single set

--- test/integration/transactiontest.go
+++ test/integration/transactiontest.go
@@ -44,25 +44,13 @@ func TestTransaction(t *testing.T) {
 	var devicePathsForSet = make([]DevicePath, 2)
 	devicePathsForSet[0].deviceName = device
 	devicePathsForSet[0].path = path1
-	devicePathsForSet[0].value = value2
-	devicePathsForSet[1].deviceName = device
-	devicePathsForSet[1].path = path2
-	devicePathsForSet[1].value = value1
-	changeID, errorSet := GNMISet(MakeContext(), c, devicePathsForSet)
-	assert.NoError(t, errorSet)
-	assert.True(t, changeID != "")
-
-	//  Trial
-	devicePathsForSet[0].deviceName = device
-	devicePathsForSet[0].path = path1
 	devicePathsForSet[0].value = value1
 	devicePathsForSet[1].deviceName = device
 	devicePathsForSet[1].path = path2
 	devicePathsForSet[1].value = value2
-	changeID, errorSet = GNMISet(MakeContext(), c, devicePathsForSet)
+	changeID, errorSet := GNMISet(MakeContext(), c, devicePathsForSet)
 	assert.NoError(t, errorSet)
 	assert.True(t, changeID != "")
-	// End Trial
 
 	var devicePathsForGet = make([]DevicePath, 2)
 	devicePathsForGet[0].deviceName = device
