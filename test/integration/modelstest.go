090309a6e8fd09c8aeeac4d855641f3094f5abfc
Ray Milkey <ray@opennetworking.org>
2019-07-24 15:47:17 -0700

    Use table driven testing paradigm

--- test/integration/modelstest.go
+++ test/integration/modelstest.go
@@ -21,65 +21,59 @@ import (
 	"testing"
 )
 
-const (
-	badPath           = "/openconfig-system:system/config/no-such-path"
-	ntpPath           = "/openconfig-system:system/ntp/state/enable-ntp-auth"
-	hostnamePath      = "/openconfig-system:system/config/hostname"
-	clockTimeZonePath = "/openconfig-system:system/clock/config/timezone-name"
-)
-
 func init() {
 	Registry.RegisterTest("models", TestModels, []*runner.TestSuite{AllTests,SomeTests,IntegrationTests})
 }
 
 // TestModels tests GNMI operation involving unknown or illegal paths
 func TestModels(t *testing.T) {
-	// Get the configured devices from the environment.
-	device1 := env.GetDevices()[0]
-	device2 := env.GetDevices()[1]
-	devices := make([]string, 2)
-	devices[0] = device1
-	devices[1] = device2
+	const (
+		unknownPath       = "/openconfig-system:system/config/no-such-path"
+		ntpPath           = "/openconfig-system:system/ntp/state/enable-ntp-auth"
+		hostNamePath      = "/openconfig-system:system/config/hostname"
+		clockTimeZonePath = "/openconfig-system:system/clock/config/timezone-name"
+	)
+
+	// Get the configured device from the environment.
+	device := env.GetDevices()[0]
+
+	// Data to run the test cases
+	testCases := []struct {
+		description   string
+		path          string
+		value         string
+		expectedError string
+	}{
+		{description: "Unknown path", path: unknownPath, value: "123456", expectedError: "JSON contains unexpected field no-such-path"},
+		{description: "Read only path", path: ntpPath, value: "bool_val:false", expectedError: "read only"},
+		{description: "Wrong type", path: clockTimeZonePath, value: "int_val:11111", expectedError: "expect string"},
+		{description: "Constraint violation", path: hostNamePath, value: "not a host name", expectedError: "does not match regular expression pattern"},
+	}
 
 	// Make a GNMI client to use for requests
 	gnmiClient, gnmiClientError := env.NewGnmiClient(MakeContext(), "gnmi")
 	assert.NoError(t, gnmiClientError)
 	assert.True(t, gnmiClient != nil, "Fetching client returned nil")
 
-	// Try to set a bad path
-	setBadPath := makeDevicePath(device1, badPath)
-	setBadPath[0].value = "123456"
-	_, errorBadSet := GNMISet(MakeContext(), gnmiClient, setBadPath, LeaveNamespaces)
-	assert.NotNil(t, errorBadSet, "Set operation on unknown path does not generate an error")
-	assert.Contains(t, errorBadSet.Error(),
-		"JSON contains unexpected field no-such-path",
-		"set operation on unknown path generates wrong error")
+	for _, testCase := range testCases {
+		t.Run(testCase.description,
+			func(t *testing.T) {
+				description := testCase.description
+				path := testCase.path
+				value := testCase.value
+				expectedError := testCase.expectedError
+				t.Parallel()
 
-	// Try to set a read-only path
-	setReadOnlyPath := makeDevicePath(device1, ntpPath)
-	setReadOnlyPath[0].value = "bool_val:false"
-	_, errorReadOnlySet := GNMISet(MakeContext(), gnmiClient, setReadOnlyPath, LeaveNamespaces)
-	assert.NotNil(t, errorReadOnlySet, "Set operation on read-only path does not generate an error")
-	assert.Contains(t, errorReadOnlySet.Error(),
-		"read only",
-		"set operation on unknown path generates wrong error")
+				t.Logf("testing %q", description)
 
-	// Try to set the wrong type
-	setWrongType := makeDevicePath(device1, clockTimeZonePath)
-	setWrongType[0].value = "int_val:11111"
-	_, errorWrongTypeSet := GNMISet(MakeContext(), gnmiClient, setWrongType, LeaveNamespaces)
-	assert.NotNil(t, errorWrongTypeSet, "Set operation with bad type does not generate an error")
-	assert.Contains(t, errorWrongTypeSet.Error(),
-		"expect string",
-		"set operation on unknown path generates wrong error")
-
-	// Try to set a value that does not match constraints
-	setWrongValue := makeDevicePath(device1, hostnamePath)
-	setWrongValue[0].value = "not a host name"
-	_, errorWrongValueSet := GNMISet(MakeContext(), gnmiClient, setWrongValue, LeaveNamespaces)
-	assert.NotNil(t, errorWrongValueSet, "Set operation with bad type does not generate an error")
-	assert.Contains(t, errorWrongValueSet.Error(),
-		"does not match regular expression pattern",
-		"set operation on unknown path generates wrong error")
+				setResult := makeDevicePath(device, path)
+				setResult[0].value = value
+				_, errorSet := GNMISet(MakeContext(), gnmiClient, setResult, LeaveNamespaces)
+				assert.NotNil(t, errorSet, "Set operation for %s does not generate an error", description)
+				assert.Contains(t, errorSet.Error(),
+					expectedError,
+					"set operation for %s generates wrong error", description)
+		})
+	}
 }
 
