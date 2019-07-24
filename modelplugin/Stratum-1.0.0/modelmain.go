bfa5db073fdc8d84150212be98e34f4720c16efb
Andrea Campanella <andrea@opennetworking.org>
2019-07-24 19:52:54 +0200

    Fixing version of yang models according to Stratum commit-id used for the hercules and openconfig repos

--- modelplugin/Stratum-1.0.0/modelmain.go
+++ modelplugin/Stratum-1.0.0/modelmain.go
@@ -32,7 +32,23 @@ const modelversion = "1.0.0"
 const modulename = "stratum.so.1.0.0"
 
 var modelData = []*gnmi.ModelData{
-      {Name: "openconfig-interfaces-stratum", Organization: "Open Networking Foundation", Version: "0.1.0"},{Name: "openconfig-lacp-stratum", Organization: "Open Networking Foundation", Version: "0.1.0"},{Name: "openconfig-platform-stratum", Organization: "Open Networking Foundation", Version: "0.1.0"},{Name: "openconfig-platform-transceiver-stratum", Organization: "Open Networking Foundation", Version: "0.1.0"},{Name: "openconfig-qos-interfaces-stratum", Organization: "Open Networking Foundation", Version: "0.1.0"},{Name: "openconfig-interfaces", Organization: "OpenConfig working group", Version: "2.4.1"},{Name: "openconfig-if-ip", Organization: "OpenConfig working group", Version: "3.0.0"},{Name: "openconfig-lacp", Organization: "OpenConfig working group", Version: "1.1.1"},{Name: "openconfig-platform", Organization: "OpenConfig working group", Version: "0.12.2"},{Name: "openconfig-platform-linecard", Organization: "OpenConfig working group", Version: "0.1.1"},{Name: "openconfig-platform-port", Organization: "OpenConfig working group", Version: "0.3.2"},{Name: "openconfig-platform-transceiver", Organization: "OpenConfig working group", Version: "0.7.0"},{Name: "openconfig-vlan", Organization: "OpenConfig working group", Version: "3.2.0"},{Name: "openconfig-system", Organization: "OpenConfig working group", Version: "0.7.0"},{Name: "openconfig-hercules-platform-linecard", Organization: "OpenConfig working group", Version: "0.2.0"},{Name: "openconfig-hercules-qos", Organization: "OpenConfig working group", Version: "0.1.0"},{Name: "openconfig-hercules-platform", Organization: "OpenConfig working group", Version: "0.2.0"},{Name: "openconfig-hercules-platform-chassis", Organization: "OpenConfig working group", Version: "0.2.0"},{Name: "openconfig-hercules-platform-port", Organization: "OpenConfig working group", Version: "0.2.0"},{Name: "openconfig-hercules", Organization: "OpenConfig working group", Version: "0.2.0"},{Name: "openconfig-hercules-interfaces", Organization: "OpenConfig working group", Version: "0.2.0"},{Name: "openconfig-hercules-platform-node", Organization: "OpenConfig working group", Version: "0.2.0"},	
+	{Name: "openconfig-interfaces", Organization: "OpenConfig working group", Version: "2.4.1"},
+	{Name: "openconfig-if-ip", Organization: "OpenConfig working group", Version: "3.0.0"},
+	{Name: "openconfig-lacp", Organization: "OpenConfig working group", Version: "1.1.1"},
+	{Name: "openconfig-platform", Organization: "OpenConfig working group", Version: "0.12.2"},
+	{Name: "openconfig-platform-linecard", Organization: "OpenConfig working group", Version: "0.1.1"},
+	{Name: "openconfig-platform-port", Organization: "OpenConfig working group", Version: "0.3.2"},
+	{Name: "openconfig-platform-transceiver", Organization: "OpenConfig working group", Version: "0.7.0"},
+	{Name: "openconfig-vlan", Organization: "OpenConfig working group", Version: "3.2.0"},
+	{Name: "openconfig-system", Organization: "OpenConfig working group", Version: "0.7.0"},
+	{Name: "openconfig-hercules-platform-linecard", Organization: "OpenConfig working group", Version: "0.2.0"},
+	{Name: "openconfig-hercules-qos", Organization: "OpenConfig working group", Version: "0.1.0"},
+	{Name: "openconfig-hercules-platform", Organization: "OpenConfig working group", Version: "0.2.0"},
+	{Name: "openconfig-hercules-platform-chassis", Organization: "OpenConfig working group", Version: "0.2.0"},
+	{Name: "openconfig-hercules-platform-port", Organization: "OpenConfig working group", Version: "0.2.0"},
+	{Name: "openconfig-hercules", Organization: "OpenConfig working group", Version: "0.2.0"},
+	{Name: "openconfig-hercules-interfaces", Organization: "OpenConfig working group", Version: "0.2.0"},
+	{Name: "openconfig-hercules-platform-node", Organization: "OpenConfig working group", Version: "0.2.0"},
 }
 
 func (m modelplugin) ModelData() (string, string, []*gnmi.ModelData, string) {
