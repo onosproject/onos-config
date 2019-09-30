0111d4c3816fcf7ba46b5712760f6956affaf0de
Andrea Campanella <andrea@opennetworking.org>
2019-09-30 10:35:24 +0200

    Hold for device reply before notifying subscribers of a change. (#701)
    * Hold for device reply before notifying subscribers of a change. Fixes #437
    
    * Unit Test, working in goland
    
    * working test
    
    * Reposting event in any case

--- pkg/northbound/gnmi/set.go
+++ pkg/northbound/gnmi/set.go
@@ -383,9 +383,13 @@ func listenForDeviceResponse(changes mapNetworkChanges, target string, name stor
 	//blocking until we receive something from the channel or for 5 seconds, whatever comes first.
 	select {
 	case response := <-respChan:
+		go func() {
+			respChan <- response
+		}()
 		switch eventType := response.EventType(); eventType {
 		case events.EventTypeAchievedSetConfig:
 			log.Info("Set is properly configured ", response.ChangeID())
+			//Passing by the event to subscribe subsystem
 			return nil
 		case events.EventTypeErrorSetConfig:
 			//Removing previously applied config
