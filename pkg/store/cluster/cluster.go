e4d8b881e6ee028d3ed48804df58ea2989c887e2
Jordan Halterman <jordan.halterman@gmail.com>
2019-10-02 17:20:56 -0700

    Add utilities for reading cluster/node information.

---
+++ pkg/store/cluster/cluster.go
@@ -0,0 +1,31 @@
+// Copyright 2019-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cluster
+
+import "os"
+
+const nodeIDEnv = "NODE_ID"
+
+// NodeID is a node identifier provided by the environment
+type NodeID string
+
+// GetNodeID gets the local node identifier
+func GetNodeID() NodeID {
+	nodeID := os.Getenv(nodeIDEnv)
+	if nodeID == "" {
+		panic("No node ID provided by environment")
+	}
+	return NodeID(nodeID)
+}
