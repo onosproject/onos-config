979a00bb20227f3c49ce76b59679c4e4697f2ca2
Jordan Halterman <jordan.halterman@gmail.com>
2019-10-03 13:18:12 -0700

    Add network and device change rollback types.

---
+++ pkg/types/device/rollback/types.go
@@ -0,0 +1,23 @@
+// Copyright 2019-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package change
+
+import "github.com/onosproject/onos-config/pkg/types"
+
+// ID is a change ID type
+type ID types.ID
+
+// Revision is a change revision number
+type Revision types.Revision
