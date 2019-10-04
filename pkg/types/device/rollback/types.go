d07f4c2af4ff9c2f25b5fac3c539ead494fec3ee
Jordan Halterman <jordan.halterman@gmail.com>
2019-10-04 13:38:18 -0700

    Add network and device rollback types

---
+++ pkg/types/device/rollback/types.go
@@ -0,0 +1,45 @@
+// Copyright 2019-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rollback
+
+import (
+	"fmt"
+	"github.com/onosproject/onos-config/pkg/types"
+	"github.com/onosproject/onos-topo/pkg/northbound/device"
+	"strings"
+)
+
+const separator = ":"
+
+// ID is a rollback identifier type
+type ID types.ID
+
+// NewID returns a new device rollback ID from the given network rollback ID, device ID, and device version
+func NewID(networkID types.ID, deviceID device.ID, version string) ID {
+	return ID(fmt.Sprintf("%s%s%s%s%s", networkID, separator, deviceID, separator, version))
+}
+
+// GetDeviceID returns the device ID from the rollback ID
+func (i ID) GetDeviceID() device.ID {
+	return device.ID(string(i)[strings.Index(string(i), separator)+1 : strings.LastIndex(string(i), separator)])
+}
+
+// GetDeviceVersion returns the device version from the rollback ID
+func (i ID) GetDeviceVersion() string {
+	return string(i)[strings.LastIndex(string(i), separator)+1:]
+}
+
+// Revision is a change revision number
+type Revision types.Revision
