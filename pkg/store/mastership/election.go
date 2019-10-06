f977f5de516b62fc6aa67355a1ff18448e93810d
Jordan Halterman <jordan.halterman@gmail.com>
2019-10-06 12:01:10 -0700

    Add device mastership store

---
+++ pkg/store/mastership/election.go
@@ -0,0 +1,179 @@
+// Copyright 2019-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mastership
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/atomix/atomix-go-client/pkg/client"
+	"github.com/atomix/atomix-go-client/pkg/client/election"
+	"github.com/atomix/atomix-go-client/pkg/client/primitive"
+	"github.com/atomix/atomix-go-client/pkg/client/session"
+	"github.com/onosproject/onos-config/pkg/store/cluster"
+	"github.com/onosproject/onos-topo/pkg/northbound/device"
+	"google.golang.org/grpc"
+	"io"
+	"sync"
+	"time"
+)
+
+// newAtomixElection returns a new persistent device mastership election
+func newAtomixElection(deviceID device.ID, group *client.PartitionGroup) (deviceMastershipElection, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	election, err := group.GetElection(ctx, fmt.Sprintf("mastership-%s", deviceID), session.WithID(string(cluster.GetNodeID())), session.WithTimeout(15*time.Second))
+	cancel()
+	if err != nil {
+		return nil, err
+	}
+	return newDeviceMastershipElection(deviceID, election)
+}
+
+// newLocalElection returns a new local device mastership election
+func newLocalElection(deviceID device.ID, nodeID cluster.NodeID, conn *grpc.ClientConn) (deviceMastershipElection, error) {
+	name := primitive.Name{
+		Namespace: "local",
+		Name:      fmt.Sprintf("mastership-%s", deviceID),
+	}
+	election, err := election.New(context.Background(), name, []*grpc.ClientConn{conn}, session.WithID(string(nodeID)), session.WithTimeout(15*time.Second))
+	if err != nil {
+		return nil, err
+	}
+	return newDeviceMastershipElection(deviceID, election)
+}
+
+// newDeviceMastershipElection creates and enters a new device mastership election
+func newDeviceMastershipElection(deviceID device.ID, election election.Election) (deviceMastershipElection, error) {
+	deviceElection := &atomixDeviceMastershipElection{
+		deviceID: deviceID,
+		election: election,
+		watchers: make([]chan<- Mastership, 0, 1),
+	}
+	if err := deviceElection.enter(); err != nil {
+		return nil, err
+	}
+	return deviceElection, nil
+}
+
+// deviceMastershipElection is an election for a single device mastership
+type deviceMastershipElection interface {
+	io.Closer
+
+	// DeviceID returns the device for which this election provides mastership
+	DeviceID() device.ID
+
+	// isMaster returns a bool indicating whether the local node is the master for the device
+	isMaster() (bool, error)
+
+	// watch watches the election for changes
+	watch(ch chan<- Mastership) error
+}
+
+// atomixDeviceMastershipElection is a persistent device mastership election
+type atomixDeviceMastershipElection struct {
+	deviceID   device.ID
+	election   election.Election
+	mastership *Mastership
+	watchers   []chan<- Mastership
+	mu         sync.RWMutex
+}
+
+func (e *atomixDeviceMastershipElection) DeviceID() device.ID {
+	return e.deviceID
+}
+
+// enter enters the election
+func (e *atomixDeviceMastershipElection) enter() error {
+	ch := make(chan *election.Event)
+	if err := e.election.Watch(context.Background(), ch); err != nil {
+		return err
+	}
+
+	// Enter the election to get the current leadership term
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	term, err := e.election.Enter(ctx)
+	cancel()
+	if err != nil {
+		_ = e.election.Close()
+		return err
+	}
+
+	// Set the mastership term
+	e.mu.Lock()
+	e.mastership = &Mastership{
+		Device: e.deviceID,
+		Master: cluster.NodeID(term.Leader),
+		Term:   Term(term.ID),
+	}
+	e.mu.Unlock()
+
+	// Wait for the election event to be received before returning
+	for event := range ch {
+		if event.Term.ID == term.ID {
+			go e.watchElection(ch)
+			return nil
+		}
+	}
+
+	_ = e.election.Close()
+	return errors.New("failed to enter election")
+}
+
+// watchElection watches the election events and updates mastership info
+func (e *atomixDeviceMastershipElection) watchElection(ch <-chan *election.Event) {
+	for event := range ch {
+		var mastership *Mastership
+		e.mu.Lock()
+		if uint64(e.mastership.Term) != event.Term.ID {
+			mastership = &Mastership{
+				Device: e.deviceID,
+				Term:   Term(event.Term.ID),
+				Master: cluster.NodeID(event.Term.Leader),
+			}
+			e.mastership = mastership
+		}
+		e.mu.Unlock()
+
+		if mastership != nil {
+			e.mu.RLock()
+			for _, watcher := range e.watchers {
+				watcher <- *mastership
+			}
+			e.mu.RUnlock()
+		}
+	}
+}
+
+func (e *atomixDeviceMastershipElection) isMaster() (bool, error) {
+	e.mu.RLock()
+	defer e.mu.RUnlock()
+	if e.mastership == nil || string(e.mastership.Master) != e.election.ID() {
+		return false, nil
+	}
+	return true, nil
+}
+
+func (e *atomixDeviceMastershipElection) watch(ch chan<- Mastership) error {
+	e.mu.Lock()
+	e.watchers = append(e.watchers, ch)
+	e.mu.Unlock()
+	return nil
+}
+
+func (e *atomixDeviceMastershipElection) Close() error {
+	return e.election.Close()
+}
+
+var _ deviceMastershipElection = &atomixDeviceMastershipElection{}
---
+++ pkg/store/mastership/store_test.go
@@ -0,0 +1,140 @@
+// Copyright 2019-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mastership
+
+import (
+	"github.com/onosproject/onos-config/pkg/store/cluster"
+	"github.com/onosproject/onos-topo/pkg/northbound/device"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMastershipStore(t *testing.T) {
+	node, conn := startLocalNode()
+
+	node1 := cluster.NodeID("node1")
+	node2 := cluster.NodeID("node2")
+	node3 := cluster.NodeID("node3")
+
+	device1 := device.ID("device1")
+	device2 := device.ID("device2")
+
+	// Create three stores for three different nodes
+	store1, err := newLocalStore(node1, conn)
+	assert.NoError(t, err)
+
+	store2, err := newLocalStore(node2, conn)
+	assert.NoError(t, err)
+
+	store3, err := newLocalStore(node3, conn)
+	assert.NoError(t, err)
+
+	// Verify that the first node that checks mastership for a device wins the election
+	// and no other node believes itself to be the master
+	master, err := store1.IsMaster(device1)
+	assert.NoError(t, err)
+	assert.True(t, master)
+
+	master, err = store2.IsMaster(device1)
+	assert.NoError(t, err)
+	assert.False(t, master)
+
+	master, err = store3.IsMaster(device1)
+	assert.NoError(t, err)
+	assert.False(t, master)
+
+	// Verify that listening for events for a device enters a node into the device mastership election
+	store2Ch2 := make(chan Mastership)
+	err = store2.Watch(device2, store2Ch2)
+	assert.NoError(t, err)
+
+	// Verify that the watching node is the master
+	master, err = store2.IsMaster(device2)
+	assert.NoError(t, err)
+	assert.True(t, master)
+
+	master, err = store1.IsMaster(device2)
+	assert.NoError(t, err)
+	assert.False(t, master)
+
+	master, err = store3.IsMaster(device2)
+	assert.NoError(t, err)
+	assert.False(t, master)
+
+	// Watch device2 mastership on an additional node and verify that it does not cause a mastership change
+	store3Ch2 := make(chan Mastership)
+	err = store3.Watch(device2, store3Ch2)
+	assert.NoError(t, err)
+
+	master, err = store3.IsMaster(device1)
+	assert.NoError(t, err)
+	assert.False(t, master)
+
+	// Listen for device1 events on remaining nodes
+	store2Ch1 := make(chan Mastership)
+	err = store2.Watch(device1, store2Ch1)
+	assert.NoError(t, err)
+	store3Ch1 := make(chan Mastership)
+	err = store3.Watch(device1, store3Ch1)
+	assert.NoError(t, err)
+
+	// Close node 1 (the master for device 1) and verify a mastership change occurs on nodes 2 and 3
+	err = store1.Close()
+	assert.NoError(t, err)
+
+	// node2 should now be the master for device1
+	mastership := <-store2Ch1
+	assert.Equal(t, device1, mastership.Device)
+	assert.Equal(t, node2, mastership.Master)
+
+	master, err = store2.IsMaster(device1)
+	assert.NoError(t, err)
+	assert.True(t, master)
+
+	// node3 should not be the master for device1
+	mastership = <-store3Ch1
+	assert.Equal(t, device1, mastership.Device)
+	assert.Equal(t, node2, mastership.Master)
+
+	master, err = store3.IsMaster(device1)
+	assert.NoError(t, err)
+	assert.False(t, master)
+
+	// Close node2 and verify mastership for both devices changes
+	err = store2.Close()
+	assert.NoError(t, err)
+
+	// node3 should now be the master for device1
+	mastership = <-store3Ch1
+	assert.Equal(t, device1, mastership.Device)
+	assert.Equal(t, node3, mastership.Master)
+
+	master, err = store3.IsMaster(device1)
+	assert.NoError(t, err)
+	assert.True(t, master)
+
+	// node3 should also be the master for device2
+	mastership = <-store3Ch2
+	assert.Equal(t, device2, mastership.Device)
+	assert.Equal(t, node3, mastership.Master)
+
+	master, err = store3.IsMaster(device2)
+	assert.NoError(t, err)
+	assert.True(t, master)
+
+	_ = store3.Close()
+	_ = conn.Close()
+	_ = node.Stop()
+}
