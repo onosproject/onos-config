4a17443d47c877c94584f7927349f7083544fea8
Jordan Halterman <jordan.halterman@gmail.com>
2019-10-08 11:52:04 -0700

    Add device watcher and tests.

--- pkg/controller/change/network/watcher.go
+++ pkg/controller/change/network/watcher.go
@@ -16,9 +16,11 @@ package network
 
 import (
 	"github.com/onosproject/onos-config/pkg/controller"
-	changestore "github.com/onosproject/onos-config/pkg/store/change/network"
+	devicechangestore "github.com/onosproject/onos-config/pkg/store/change/device"
+	networkchangestore "github.com/onosproject/onos-config/pkg/store/change/network"
 	"github.com/onosproject/onos-config/pkg/types"
-	changetype "github.com/onosproject/onos-config/pkg/types/change/network"
+	devicechangetype "github.com/onosproject/onos-config/pkg/types/change/device"
+	networkchangetype "github.com/onosproject/onos-config/pkg/types/change/network"
 	"sync"
 )
 
@@ -26,19 +28,19 @@ const queueSize = 100
 
 // Watcher is a network change watcher
 type Watcher struct {
-	Store changestore.Store
-	ch    chan *changetype.NetworkChange
+	Store networkchangestore.Store
+	ch    chan *networkchangetype.NetworkChange
 	mu    sync.Mutex
 }
 
-// Start starts the change watcher
+// Start starts the network change watcher
 func (w *Watcher) Start(ch chan<- types.ID) error {
 	w.mu.Lock()
 	if w.ch != nil {
 		return nil
 	}
 
-	configCh := make(chan *changetype.NetworkChange, queueSize)
+	configCh := make(chan *networkchangetype.NetworkChange, queueSize)
 	w.ch = configCh
 	w.mu.Unlock()
 
@@ -55,8 +57,48 @@ func (w *Watcher) Start(ch chan<- types.ID) error {
 	return nil
 }
 
-// Stop stops the change watcher
+// Stop stops the network change watcher
 func (w *Watcher) Stop() {
+	w.mu.Lock()
+	close(w.ch)
+	w.mu.Unlock()
+}
+
+var _ controller.Watcher = &Watcher{}
+
+// DeviceWatcher is a device change watcher
+type DeviceWatcher struct {
+	Store devicechangestore.Store
+	ch    chan *devicechangetype.Change
+	mu    sync.Mutex
+}
+
+// Start starts the device change watcher
+func (w *DeviceWatcher) Start(ch chan<- types.ID) error {
+	w.mu.Lock()
+	if w.ch != nil {
+		return nil
+	}
+
+	configCh := make(chan *devicechangetype.Change, queueSize)
+	w.ch = configCh
+	w.mu.Unlock()
+
+	if err := w.Store.Watch(configCh); err != nil {
+		return err
+	}
+
+	go func() {
+		defer close(ch)
+		for request := range configCh {
+			ch <- types.ID(request.NetworkChangeID)
+		}
+	}()
+	return nil
+}
+
+// Stop stops the device change watcher
+func (w *DeviceWatcher) Stop() {
 	w.mu.Lock()
 	close(w.ch)
 	w.mu.Unlock()
--- pkg/controller/change/network/watcher_test.go
+++ pkg/controller/change/network/watcher_test.go
@@ -15,7 +15,8 @@
 package network
 
 import (
-	changestore "github.com/onosproject/onos-config/pkg/store/change/network"
+	devicestore "github.com/onosproject/onos-config/pkg/store/change/device"
+	networkstore "github.com/onosproject/onos-config/pkg/store/change/network"
 	"github.com/onosproject/onos-config/pkg/types"
 	"github.com/onosproject/onos-config/pkg/types/change"
 	devicechange "github.com/onosproject/onos-config/pkg/types/change/device"
@@ -26,8 +27,8 @@ import (
 	"time"
 )
 
-func TestWatcher(t *testing.T) {
-	store, err := changestore.NewLocalStore()
+func TestNetworkWatcher(t *testing.T) {
+	store, err := networkstore.NewLocalStore()
 	assert.NoError(t, err)
 
 	watcher := &Watcher{
@@ -110,6 +111,79 @@ func TestWatcher(t *testing.T) {
 	select {
 	case id := <-ch:
 		assert.Equal(t, change1.ID, networkchange.ID(id))
+	case <-time.After(5 * time.Second):
+		t.FailNow()
+	}
+}
+
+func TestDeviceWatcher(t *testing.T) {
+	store, err := devicestore.NewLocalStore()
+	assert.NoError(t, err)
+
+	watcher := &DeviceWatcher{
+		Store: store,
+	}
+
+	ch := make(chan types.ID)
+	err = watcher.Start(ch)
+	assert.NoError(t, err)
+
+	change1 := &devicechange.Change{
+		NetworkChangeID: types.ID("network:1"),
+		DeviceID:        device.ID("device-1"),
+		Values: []*devicechange.Value{
+			{
+				Path:  "foo",
+				Value: []byte("Hello world!"),
+				Type:  devicechange.ValueType_STRING,
+			},
+			{
+				Path:  "bar",
+				Value: []byte("Hello world again!"),
+				Type:  devicechange.ValueType_STRING,
+			},
+		},
+	}
+
+	err = store.Create(change1)
+	assert.NoError(t, err)
+
+	select {
+	case id := <-ch:
+		assert.Equal(t, change1.NetworkChangeID, id)
+	case <-time.After(5 * time.Second):
+		t.FailNow()
+	}
+
+	change2 := &devicechange.Change{
+		NetworkChangeID: types.ID("network:2"),
+		DeviceID:        device.ID("device-2"),
+		Values: []*devicechange.Value{
+			{
+				Path:  "baz",
+				Value: []byte("Goodbye world!"),
+				Type:  devicechange.ValueType_STRING,
+			},
+		},
+	}
+
+	err = store.Create(change2)
+	assert.NoError(t, err)
+
+	select {
+	case id := <-ch:
+		assert.Equal(t, change2.NetworkChangeID, id)
+	case <-time.After(5 * time.Second):
+		t.FailNow()
+	}
+
+	change1.Status.State = change.State_APPLYING
+	err = store.Update(change1)
+	assert.NoError(t, err)
+
+	select {
+	case id := <-ch:
+		assert.Equal(t, change1.NetworkChangeID, id)
 	case <-time.After(5 * time.Second):
 		t.FailNow()
 	}
