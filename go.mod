e3eee7f21d3654edd640954be3a05c87ffed0e86
Jordan Halterman <jordan.halterman@gmail.com>
2019-10-08 03:20:24 -0700

    Add network change store (#728)
    * Add network change store implementation.
    
    * Add tests for network change store.
    
    * Fix linter errors.
    
    * Timeout channel reads in network change store tests.
    
    * Update status constants in network change store tests.

--- go.mod
+++ go.mod
@@ -11,14 +11,13 @@ require (
 	github.com/gogo/protobuf v1.2.2-0.20190723190241-65acae22fc9d
 	github.com/golang/mock v1.3.1
 	github.com/golang/protobuf v1.3.2
+	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/onosproject/onos-control v0.0.0-20190715190020-706a2ee0d37b // indirect
 	github.com/onosproject/onos-topo v0.0.0-20191007162302-b2f95b144981
 	github.com/openconfig/gnmi v0.0.0-20180912164834-33a1865c3029
 	github.com/openconfig/goyang v0.0.0-20190408185115-e8b0ed2cbb0c
 	github.com/openconfig/ygot v0.5.1-0.20190427030428-68346f97239f
 	github.com/pkg/errors v0.8.1
-	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/viper v1.4.0
 	github.com/stretchr/testify v1.4.0
---
+++ pkg/store/change/network/store_test.go
@@ -0,0 +1,197 @@
+// Copyright 2019-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package network
+
+import (
+	"github.com/onosproject/onos-config/pkg/types/change"
+	devicechange "github.com/onosproject/onos-config/pkg/types/change/device"
+	networkchange "github.com/onosproject/onos-config/pkg/types/change/network"
+	"github.com/onosproject/onos-topo/pkg/northbound/device"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestDeviceStore(t *testing.T) {
+	node, conn := startLocalNode()
+	defer node.Stop()
+	defer conn.Close()
+
+	store1, err := newLocalStore(conn)
+	assert.NoError(t, err)
+	defer store1.Close()
+
+	store2, err := newLocalStore(conn)
+	assert.NoError(t, err)
+	defer store2.Close()
+
+	device1 := device.ID("device-1")
+	device2 := device.ID("device-2")
+
+	ch := make(chan *networkchange.NetworkChange)
+	err = store2.Watch(ch)
+	assert.NoError(t, err)
+
+	change1 := &networkchange.NetworkChange{
+		Changes: []*devicechange.Change{
+			{
+
+				DeviceID: device1,
+				Values: []*devicechange.Value{
+					{
+						Path:  "foo",
+						Value: []byte("Hello world!"),
+						Type:  devicechange.ValueType_STRING,
+					},
+					{
+						Path:  "bar",
+						Value: []byte("Hello world again!"),
+						Type:  devicechange.ValueType_STRING,
+					},
+				},
+			},
+			{
+				DeviceID: device2,
+				Values: []*devicechange.Value{
+					{
+						Path:  "baz",
+						Value: []byte("Goodbye world!"),
+						Type:  devicechange.ValueType_STRING,
+					},
+				},
+			},
+		},
+	}
+
+	change2 := &networkchange.NetworkChange{
+		Changes: []*devicechange.Change{
+			{
+				DeviceID: device1,
+				Values: []*devicechange.Value{
+					{
+						Path:    "foo",
+						Removed: true,
+					},
+				},
+			},
+		},
+	}
+
+	// Create a new change
+	err = store1.Create(change1)
+	assert.NoError(t, err)
+	assert.Equal(t, networkchange.ID("network:1"), change1.ID)
+	assert.Equal(t, networkchange.Index(1), change1.Index)
+	assert.NotEqual(t, networkchange.Revision(0), change1.Revision)
+
+	// Get the change
+	change1, err = store2.Get("network:1")
+	assert.NoError(t, err)
+	assert.NotNil(t, change1)
+	assert.Equal(t, networkchange.ID("network:1"), change1.ID)
+	assert.Equal(t, networkchange.Index(1), change1.Index)
+	assert.NotEqual(t, networkchange.Revision(0), change1.Revision)
+
+	// Create another change
+	err = store2.Create(change2)
+	assert.NoError(t, err)
+	assert.Equal(t, networkchange.ID("network:2"), change2.ID)
+	assert.Equal(t, networkchange.Index(2), change2.Index)
+	assert.NotEqual(t, networkchange.Revision(0), change2.Revision)
+
+	// Verify events were received for the changes
+	select {
+	case e := <-ch:
+		assert.Equal(t, networkchange.ID("network:1"), e.ID)
+	case <-time.After(5 * time.Second):
+		assert.Fail(t, "channel timed out")
+	}
+	select {
+	case e := <-ch:
+		assert.Equal(t, networkchange.ID("network:2"), e.ID)
+	case <-time.After(5 * time.Second):
+		assert.Fail(t, "channel timed out")
+	}
+
+	// Update one of the changes
+	change2.Status.State = change.State_APPLYING
+	revision := change2.Revision
+	err = store1.Update(change2)
+	assert.NoError(t, err)
+	assert.NotEqual(t, revision, change2.Revision)
+
+	// Read and then update the change
+	change2, err = store2.Get("network:2")
+	assert.NoError(t, err)
+	assert.NotNil(t, change2)
+	change2.Status.State = change.State_SUCCEEDED
+	revision = change2.Revision
+	err = store1.Update(change2)
+	assert.NoError(t, err)
+	assert.NotEqual(t, revision, change2.Revision)
+
+	// Verify that concurrent updates fail
+	change11, err := store1.Get("network:1")
+	assert.NoError(t, err)
+	change12, err := store2.Get("network:1")
+	assert.NoError(t, err)
+
+	change11.Status.State = change.State_SUCCEEDED
+	err = store1.Update(change11)
+	assert.NoError(t, err)
+
+	change12.Status.State = change.State_FAILED
+	err = store2.Update(change12)
+	assert.Error(t, err)
+
+	// Verify events were received again
+	select {
+	case e := <-ch:
+		assert.Equal(t, networkchange.ID("network:2"), e.ID)
+	case <-time.After(5 * time.Second):
+		assert.Fail(t, "channel timed out")
+	}
+	select {
+	case e := <-ch:
+		assert.Equal(t, networkchange.ID("network:2"), e.ID)
+	case <-time.After(5 * time.Second):
+		assert.Fail(t, "channel timed out")
+	}
+	select {
+	case e := <-ch:
+		assert.Equal(t, networkchange.ID("network:1"), e.ID)
+	case <-time.After(5 * time.Second):
+		assert.Fail(t, "channel timed out")
+	}
+
+	// List the changes
+	changes := make(chan *networkchange.NetworkChange)
+	err = store1.List(changes)
+	assert.NoError(t, err)
+
+	_, ok := <-changes
+	assert.True(t, ok)
+	_, ok = <-changes
+	assert.True(t, ok)
+	_, ok = <-changes
+	assert.False(t, ok)
+
+	// Delete a change
+	err = store1.Delete(change2)
+	assert.NoError(t, err)
+	change2, err = store2.Get("network:2")
+	assert.NoError(t, err)
+	assert.Nil(t, change2)
+}
