744c7f5a79eb163106804fa7c02d53a88519db01
Jordan Halterman <jordan.halterman@gmail.com>
2019-10-09 10:34:55 -0700

    Add device change partitioner and tests

---
+++ pkg/controller/change/device/partitioner_test.go
@@ -0,0 +1,31 @@
+// Copyright 2019-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package device
+
+import (
+	"github.com/onosproject/onos-config/pkg/controller"
+	"github.com/onosproject/onos-config/pkg/types"
+	devicechange "github.com/onosproject/onos-config/pkg/types/change/device"
+	"github.com/onosproject/onos-topo/pkg/northbound/device"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDevicePartitioner(t *testing.T) {
+	partitioner := &Partitioner{}
+	key, err := partitioner.Partition(types.ID(devicechange.Index(1).GetChangeID(device.ID("device-1"))))
+	assert.NoError(t, err)
+	assert.Equal(t, controller.PartitionKey("device-1"), key)
+}
