df225d4b1d8e784d948ba05f6a88c5f2debb48f4
adibrastegarnia <adib.rastegarnia@gmail.com>
2019-11-15 23:26:54 -0500

    Test Coverage

---
+++ api/types/device/types_test.go
@@ -0,0 +1,32 @@
+// Copyright 2019-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package device
+
+import (
+	"testing"
+
+	"gotest.tools/assert"
+)
+
+const (
+	testDeviceID      = "device-1"
+	testDeviceVersion = "1.0.0"
+)
+
+func TestNewVersionedID(t *testing.T) {
+	versionID := NewVersionedID(testDeviceID, testDeviceVersion)
+	assert.Equal(t, versionID.GetID(), ID(testDeviceID))
+	assert.Equal(t, versionID.GetVersion(), Version(testDeviceVersion))
+}
