8c6dc23b3e59e2afc9e886d4259745801eea5eb4
Ray Milkey <ray@opennetworking.org>
2019-12-18 16:43:52 -0800

    Add a func to wait for a network change to complete

--- test/gnmi/gnmiutils.go
+++ test/gnmi/gnmiutils.go
@@ -18,12 +18,19 @@ import (
 	"context"
 	"fmt"
 	"github.com/golang/protobuf/proto"
+	"github.com/onosproject/onos-config/api/diags"
+	"github.com/onosproject/onos-config/api/types/change"
+	"github.com/onosproject/onos-config/api/types/change/network"
 	"github.com/onosproject/onos-config/pkg/utils"
+	"github.com/onosproject/onos-test/pkg/onit/env"
 	"github.com/openconfig/gnmi/client"
 	gclient "github.com/openconfig/gnmi/client/gnmi"
 	gpb "github.com/openconfig/gnmi/proto/gnmi"
 	"github.com/openconfig/gnmi/proto/gnmi_ext"
+	"github.com/stretchr/testify/assert"
 	"strings"
+	"testing"
+	"time"
 )
 
 // DevicePath describes the results of a get operation for a single path
@@ -117,4 +124,34 @@ func MakeContext() context.Context {
 	// TODO: Investigate using context.WithCancel() here
 	ctx := context.Background()
 	return ctx
+}
+
+// IsNetworkChangeComplete checks for a COMPLETED status on the given change, retying for 15 seconds
+func IsNetworkChangeComplete(t *testing.T, networkChangeID network.ID) bool {
+	listNetworkChangeRequest := &diags.ListNetworkChangeRequest{
+		Subscribe:     false,
+		ChangeID:      networkChangeID,
+		WithoutReplay: false,
+	}
+
+	changeServiceClient, changeServiceClientErr := env.Config().NewChangeServiceClient()
+	assert.Nil(t, changeServiceClientErr)
+	assert.True(t, changeServiceClient != nil)
+
+	for i := 1; i < 5; i++ {
+		// Check if the network change has completed
+
+		listNetworkChangesClient, listNetworkChangesClientErr := changeServiceClient.ListNetworkChanges(context.Background(), listNetworkChangeRequest)
+		assert.Nil(t, listNetworkChangesClientErr)
+		assert.True(t, listNetworkChangesClient != nil)
+		networkChangeResponse, networkChangeResponseErr := listNetworkChangesClient.Recv()
+		assert.Nil(t, networkChangeResponseErr)
+		assert.True(t, networkChangeResponse != nil)
+		if change.State_COMPLETE == networkChangeResponse.Change.Status.State {
+			return true
+		}
+
+		time.Sleep(2*time.Second)
+	}
+	return false
 }
