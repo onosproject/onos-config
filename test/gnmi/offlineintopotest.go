7ba7a57f5fbc057ddd454ce640b3027824cc541f
Ray Milkey <ray@opennetworking.org>
2019-12-20 10:22:17 -0800

    Add integration test for set() is called on a device in the topo but offline (#973)
    * Add integration test for scenario where a set() is called on a device in the topo but offline
    
    * Add a more desciptive comment to the test method
    
    * Refactor a bit based on review comments
    
    * Check for PENDING state on network change
    
    * Check for COMPLETE state on network change when simulator has been started
    
    * upgrade onos-test version
    
    * Fix subscribe and some timing problems
    
    * clean up
    
    * Remove custom onos-test module
    
    * gofmt

---
+++ test/gnmi/offlineintopotest.go
@@ -0,0 +1,114 @@
+// Copyright 2019-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package gnmi
+
+import (
+	"context"
+	"github.com/onosproject/onos-config/api/diags"
+	"github.com/onosproject/onos-config/api/types/change"
+	"github.com/onosproject/onos-config/api/types/change/network"
+	"github.com/onosproject/onos-config/pkg/northbound/gnmi"
+	"github.com/onosproject/onos-test/pkg/onit/env"
+	"github.com/onosproject/onos-topo/api/device"
+	"github.com/stretchr/testify/assert"
+	"strconv"
+	"testing"
+	"time"
+)
+
+const (
+	offlineInTopoModPath          = "/system/clock/config/timezone-name"
+	offlineInTopoModValue         = "Europe/Rome"
+	offlineInTopoModDeviceName    = "offline-dev-1"
+	offlineInTopoModDeviceVersion = "1.0.0"
+	offlineInTopoModDeviceType    = "Devicesim"
+)
+
+// TestOfflineDeviceInTopo tests set/query of a single GNMI path to a single device that is in the config but offline
+func (s *TestSuite) TestOfflineDeviceInTopo(t *testing.T) {
+	deviceClient, deviceClientError := env.Topo().NewDeviceServiceClient()
+	assert.NotNil(t, deviceClient)
+	assert.Nil(t, deviceClientError)
+	timeout := 10 * time.Second
+	newDevice := &device.Device{
+		ID:      offlineInTopoModDeviceName,
+		Address: offlineInTopoModDeviceName + ":11161",
+		Type:    offlineInTopoModDeviceType,
+		Version: offlineInTopoModDeviceVersion,
+		Timeout: &timeout,
+		TLS: device.TlsConfig{
+			Plain: true,
+		},
+	}
+	addRequest := &device.AddRequest{Device: newDevice}
+	addResponse, addResponseError := deviceClient.Add(context.Background(), addRequest)
+	assert.NotNil(t, addResponse)
+	assert.Nil(t, addResponseError)
+
+	// Make a GNMI client to use for requests
+	c, err := env.Config().NewGNMIClient()
+	assert.NoError(t, err)
+	assert.True(t, c != nil, "Fetching client returned nil")
+
+	// Set a value using gNMI client to the offline device
+
+	setPath := makeDevicePath(offlineInTopoModDeviceName, offlineInTopoModPath)
+	setPath[0].pathDataValue = offlineInTopoModValue
+	setPath[0].pathDataType = StringVal
+
+	_, extensionsSet, errorSet := gNMISet(MakeContext(), c, setPath, noPaths, noExtensions)
+	assert.NoError(t, errorSet)
+	assert.Equal(t, 1, len(extensionsSet))
+	extensionBefore := extensionsSet[0].GetRegisteredExt()
+	assert.Equal(t, extensionBefore.Id.String(), strconv.Itoa(gnmi.GnmiExtensionNetwkChangeID))
+	networkChangeID := network.ID(extensionBefore.Msg)
+
+	// Check that the value was set correctly
+	valueAfter, extensions, errorAfter := gNMIGet(MakeContext(), c, makeDevicePath(offlineInTopoModDeviceName, offlineInTopoModPath))
+	assert.NoError(t, errorAfter)
+	assert.Equal(t, 0, len(extensions))
+	assert.NotEqual(t, "", valueAfter, "Query after set returned an error: %s\n", errorAfter)
+	assert.Equal(t, offlineInTopoModValue, valueAfter[0].pathDataValue, "Query after set returned the wrong value: %s\n", valueAfter)
+
+	// Check for pending state on the network change
+	changeServiceClient, changeServiceClientErr := env.Config().NewChangeServiceClient()
+	assert.Nil(t, changeServiceClientErr)
+	assert.True(t, changeServiceClient != nil)
+	listNetworkChangeRequest := &diags.ListNetworkChangeRequest{
+		Subscribe:     true,
+		ChangeID:      networkChangeID,
+		WithoutReplay: false,
+	}
+	listNetworkChangesClient, listNetworkChangesClientErr := changeServiceClient.ListNetworkChanges(context.Background(), listNetworkChangeRequest)
+	assert.Nil(t, listNetworkChangesClientErr)
+	assert.True(t, listNetworkChangesClient != nil)
+	networkChangeResponse, networkChangeResponseErr := listNetworkChangesClient.Recv()
+	assert.Nil(t, networkChangeResponseErr)
+	assert.True(t, networkChangeResponse != nil)
+	assert.Equal(t, change.State_PENDING, networkChangeResponse.Change.Status.State)
+
+	// Start the device simulator
+	simulator := env.NewSimulator().SetName(offlineInTopoModDeviceName).SetAddDevice(false)
+	simulatorEnv := simulator.AddOrDie()
+	time.Sleep(100 * time.Second)
+
+	// Check that the network change has completed
+	WaitForNetworkChangeComplete(t, networkChangeID)
+
+	// Interrogate the device to check that the value was set properly
+	deviceGnmiClient := getDeviceGNMIClient(t, simulatorEnv)
+	checkDeviceValue(t, deviceGnmiClient, makeDevicePath(offlineInTopoModDeviceName, offlineInTopoModPath), offlineInTopoModValue)
+}
