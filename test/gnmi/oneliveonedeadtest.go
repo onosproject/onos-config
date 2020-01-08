97f0dbda6114a8eb74b3861bcbb2ddea479fd729
Ray Milkey <ray@opennetworking.org>
2020-01-08 12:28:06 -0800

    Refactor creating extensions into a utility routine; clean up constants

--- test/gnmi/oneliveonedeadtest.go
+++ test/gnmi/oneliveonedeadtest.go
@@ -16,59 +16,41 @@
 package gnmi
 
 import (
-	"github.com/onosproject/onos-config/pkg/northbound/gnmi"
 	"github.com/onosproject/onos-test/pkg/onit/env"
-	"github.com/openconfig/gnmi/proto/gnmi_ext"
 	"testing"
-)
-
-const (
-	oneLiveOneDeadDeviceModPath          = "/system/clock/config/timezone-name"
-	oneLiveOneDeadDeviceModValue         = "Europe/Rome"
-	oneLiveOneDeadDeviceName    = "dead-dev-1"
-	oneLiveOneDeadDeviceVersion = "1.0.0"
-	oneLiveOneDeadDeviceType    = "Devicesim"
-	oneLiveOneDeadDeviceAddress          = "198.18.0.1:4"
 )
 
 // TestOneLiveOneDeadDevice tests GNMI operations to an offline device followed by operations to a connected device
 func (s *TestSuite) TestOneLiveOneDeadDevice(t *testing.T) {
 
+	const (
+		modPath  = "/system/clock/config/timezone-name"
+		modValue = "Europe/Rome"
+	)
+
 	// Make a GNMI client to use for requests
 	gnmiClient := getGNMIClientOrFail(t)
 
 	// Set a value using gNMI client to the offline device
-	extNameDeviceType := gnmi_ext.Extension_RegisteredExt{
-		RegisteredExt: &gnmi_ext.RegisteredExtension{
-			Id:  gnmi.GnmiExtensionDeviceType,
-			Msg: []byte(oneLiveOneDeadDeviceType),
-		},
-	}
-	extNameDeviceVersion := gnmi_ext.Extension_RegisteredExt{
-		RegisteredExt: &gnmi_ext.RegisteredExtension{
-			Id:  gnmi.GnmiExtensionVersion,
-			Msg: []byte(oneLiveOneDeadDeviceVersion),
-		},
-	}
-	extensions := []*gnmi_ext.Extension{{Ext: &extNameDeviceType}, {Ext: &extNameDeviceVersion}}
-
-	offlineDevicePath := getDevicePathWithValue("offline-device", oneLiveOneDeadDeviceModPath, oneLiveOneDeadDeviceModValue, StringVal)
+	offlineDevicePath := getDevicePathWithValue("offline-device", modPath, modValue, StringVal)
 
 	// Set the value - should return a pending change
-	setGNMIValueOrFail(t, gnmiClient, offlineDevicePath, noPaths, extensions)
+	setGNMIValueOrFail(t, gnmiClient, offlineDevicePath, noPaths, getSimulatorExtensions())
 
 	// Check that the value was set correctly in the cache
-	checkGNMIValue(t, gnmiClient, offlineDevicePath, oneLiveOneDeadDeviceModValue, 0, "Query after set returned the wrong value")
+	checkGNMIValue(t, gnmiClient, offlineDevicePath, modValue, 0, "Query after set returned the wrong value")
 
 	// Create an online device
-	simulator := env.NewSimulator().AddOrDie()
+	onlineSimulator := env.NewSimulator().AddOrDie()
 
 	// Set a value to the online device
-	onlineDevicePath := getDevicePathWithValue(simulator.Name(), oneLiveOneDeadDeviceModPath, oneLiveOneDeadDeviceModValue, StringVal)
-
-	// Set a value using gNMI client
+	onlineDevicePath := getDevicePathWithValue(onlineSimulator.Name(), modPath, modValue, StringVal)
 	setGNMIValueOrFail(t, gnmiClient, onlineDevicePath, noPaths, noExtensions)
 
-	// Check that the value was set correctly
-	checkGNMIValue(t, gnmiClient, onlineDevicePath, oneLiveOneDeadDeviceModValue, 0, "Query after set returned the wrong value")
+	// Check that the value was set correctly in the cache
+	checkGNMIValue(t, gnmiClient, onlineDevicePath, modValue, 0, "Query after set returned the wrong value")
+
+	// Check that the value was set correctly on the device
+	deviceGnmiClient := getDeviceGNMIClientOrFail(t, onlineSimulator)
+	checkDeviceValue(t, deviceGnmiClient, onlineDevicePath, modValue)
 }
