5a8a046c77573710b27fc576720eb1281084f278
Sean Condon <sean@opennetworking.org>
2020-01-10 15:26:08 +0000

    Added error handling to the GetSnapshot RPC on the Admin NBI Fixes #864 (#1026)

--- pkg/northbound/admin/admin_test.go
+++ pkg/northbound/admin/admin_test.go
@@ -17,8 +17,12 @@ package admin
 import (
 	"context"
 	"errors"
+	"fmt"
 	"github.com/golang/mock/gomock"
 	"github.com/onosproject/onos-config/api/admin"
+	device2 "github.com/onosproject/onos-config/api/types/change/device"
+	"github.com/onosproject/onos-config/api/types/device"
+	devicesnapshot "github.com/onosproject/onos-config/api/types/snapshot/device"
 	"github.com/onosproject/onos-config/pkg/manager"
 	mockstore "github.com/onosproject/onos-config/pkg/test/mocks/store"
 	"github.com/onosproject/onos-config/pkg/test/mocks/store/cache"
@@ -101,4 +105,65 @@ func Test_RollbackNetworkChange_NoChange(t *testing.T) {
 	mockNwChStore.EXPECT().Get(gomock.Any()).Return(nil, errors.New("change is not specified"))
 	_, err := client.RollbackNetworkChange(context.Background(), &admin.RollbackRequest{Name: ""})
 	assert.ErrorContains(t, err, "is not")
+}
+
+func Test_GetSnapshot(t *testing.T) {
+	mgrTest, conn, client, server := setUpServer(t)
+	defer server.Stop()
+	defer conn.Close()
+
+	const dev1ID = "presentdevice"
+	const dev1Ver = "2.0.0"
+	snapshotID := devicesnapshot.ID(fmt.Sprintf("Snapshot:%s:%s", dev1ID, dev1Ver))
+
+	mockDevSnapshotStore, ok := mgrTest.DeviceSnapshotStore.(*mockstore.MockDeviceSnapshotStore)
+	assert.Assert(t, ok, "casting mock store")
+
+	mockDevSnapshotStore.EXPECT().Load(gomock.Any()).DoAndReturn(
+		func(devId device.VersionedID) (*devicesnapshot.Snapshot, error) {
+			if devId.GetID() == dev1ID && devId.GetVersion() == dev1Ver {
+				return &devicesnapshot.Snapshot{
+					ID:            snapshotID,
+					DeviceID:      dev1ID,
+					DeviceVersion: dev1Ver,
+					DeviceType:    "TestDevice",
+					SnapshotID:    "test-snapshot",
+					ChangeIndex:   0,
+					Values: []*device2.PathValue{
+						{Path: "/a/b/c", Value: device2.NewTypedValueInt64(12345)},
+						{Path: "/a/b/d", Value: device2.NewTypedValueInt64(12346)},
+					},
+				}, nil
+			}
+			return nil, nil
+		}).AnyTimes()
+
+	snapshot, err := client.GetSnapshot(context.Background(), &admin.GetSnapshotRequest{
+		DeviceID:      device.ID("testdevice"),
+		DeviceVersion: device.Version(""),
+	})
+	assert.ErrorContains(t, err, "An ID and Version must be given")
+	assert.Assert(t, snapshot == nil, "Expecting snapshot to be nil")
+
+	// Try again with a version
+	snapshot, err = client.GetSnapshot(context.Background(), &admin.GetSnapshotRequest{
+		DeviceID:      device.ID("absentdevice"),
+		DeviceVersion: device.Version("1.0.0"),
+	})
+	assert.ErrorContains(t, err, "No snapshot found")
+	assert.Assert(t, snapshot == nil, "Expecting snapshot to be nil")
+
+	// Try again with a positive response
+	snapshot, err = client.GetSnapshot(context.Background(), &admin.GetSnapshotRequest{
+		DeviceID:      dev1ID,
+		DeviceVersion: dev1Ver,
+	})
+	assert.NilError(t, err, "Not expecting error on GetSnapshot")
+
+	assert.Equal(t, string(snapshotID), string(snapshot.ID))
+	assert.Equal(t, dev1ID, string(snapshot.DeviceID))
+	assert.Equal(t, dev1Ver, string(snapshot.DeviceVersion))
+	assert.Equal(t, "TestDevice", string(snapshot.DeviceType))
+	assert.Equal(t, "test-snapshot", string(snapshot.SnapshotID))
+	assert.Equal(t, 2, len(snapshot.GetValues()))
 }
