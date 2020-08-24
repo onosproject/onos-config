5fbc10872926be9983565b6af0a62b69eb4b3855
adibrastegarnia <adib.rastegarnia@gmail.com>
2020-08-24 13:51:27 -0700

    Refactor NewManager to initialize stores inside its function

--- cmd/onos-config/onos-config.go
+++ cmd/onos-config/onos-config.go
@@ -37,24 +37,12 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"os"
 	"time"
 
-	"github.com/onosproject/onos-config/pkg/config"
 	"github.com/onosproject/onos-config/pkg/manager"
 	"github.com/onosproject/onos-config/pkg/northbound/admin"
 	"github.com/onosproject/onos-config/pkg/northbound/diags"
 	"github.com/onosproject/onos-config/pkg/northbound/gnmi"
-	"github.com/onosproject/onos-config/pkg/store/change/device"
-	"github.com/onosproject/onos-config/pkg/store/change/device/state"
-	"github.com/onosproject/onos-config/pkg/store/change/network"
-	devicestore "github.com/onosproject/onos-config/pkg/store/device"
-	"github.com/onosproject/onos-config/pkg/store/device/cache"
-	"github.com/onosproject/onos-config/pkg/store/leadership"
-	"github.com/onosproject/onos-config/pkg/store/mastership"
-	devicesnap "github.com/onosproject/onos-config/pkg/store/snapshot/device"
-	networksnap "github.com/onosproject/onos-config/pkg/store/snapshot/network"
 	"github.com/onosproject/onos-lib-go/pkg/certs"
 	"github.com/onosproject/onos-lib-go/pkg/logging"
 	"github.com/onosproject/onos-lib-go/pkg/northbound"
@@ -86,12 +74,6 @@ func main() {
 	flag.Bool("debug", false, "enable debug logging")
 	flag.Parse()
 
-	config, err := config.GetConfig()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
 	log.Info("Starting onos-config")
 
 	opts, err := certs.HandleCertPaths(*caPath, *keyPath, *certPath, true)
@@ -99,58 +81,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	leadershipStore, err := leadership.NewAtomixStore(config)
-	if err != nil {
-		log.Error("Cannot load leadership atomix store ", err)
-	}
-
-	mastershipStore, err := mastership.NewAtomixStore(config)
-	if err != nil {
-		log.Error("Cannot load mastership atomix store ", err)
-	}
-
-	deviceChangesStore, err := device.NewAtomixStore(config)
-	if err != nil {
-		log.Error("Cannot load device atomix store ", err)
-	}
-
-	networkChangesStore, err := network.NewAtomixStore(config)
-	if err != nil {
-		log.Error("Cannot load network atomix store ", err)
-	}
-
-	networkSnapshotStore, err := networksnap.NewAtomixStore(config)
-	if err != nil {
-		log.Error("Cannot load network snapshot atomix store ", err)
-	}
-
-	deviceSnapshotStore, err := devicesnap.NewAtomixStore(config)
-	if err != nil {
-		log.Error("Cannot load network atomix store ", err)
-	}
-
-	deviceStateStore, err := state.NewStore(networkChangesStore, deviceSnapshotStore)
-	if err != nil {
-		log.Errorf("Cannot load device store with address %s:", *topoEndpoint, err)
-	}
-	log.Infof("Topology service connected with endpoint %s", *topoEndpoint)
-
-	log.Info("Network Configuration store connected")
-
-	deviceCache, err := cache.NewCache(networkChangesStore, deviceSnapshotStore)
-	if err != nil {
-		log.Error("Cannot load device cache", err)
-	}
-
-	deviceStore, err := devicestore.NewTopoStore(*topoEndpoint, opts...)
-	if err != nil {
-		log.Errorf("Cannot load device store with address %s:", *topoEndpoint, err)
-	}
-	log.Infof("Topology service connected with endpoint %s", *topoEndpoint)
-
-	mgr, err := manager.NewManager(leadershipStore, mastershipStore, deviceChangesStore,
-		deviceStateStore, deviceStore, deviceCache, networkChangesStore, networkSnapshotStore,
-		deviceSnapshotStore, *allowUnvalidatedConfig)
+	mgr, err := manager.NewManager(topoEndpoint, opts, *allowUnvalidatedConfig)
 	log.Info("Manager started")
 
 	if err != nil {
