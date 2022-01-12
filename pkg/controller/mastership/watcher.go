4bb0a2e8523bf67847bd4b72ea4436d5f028727c
Adib Rastegarnia <adib.rastegarnia@gmail.com>
2022-01-12 09:09:54 -0800

    Add mastership controller (#1406)
    * Add mastership controller
    
    * fix linter

---
+++ pkg/controller/mastership/watcher.go
@@ -0,0 +1,85 @@
+// Copyright 2021-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mastership
+
+import (
+	"context"
+	"sync"
+
+	"github.com/onosproject/onos-config/pkg/store/topo"
+
+	topoapi "github.com/onosproject/onos-api/go/onos/topo"
+	"github.com/onosproject/onos-lib-go/pkg/controller"
+)
+
+const queueSize = 100
+
+// TopoWatcher is a topology watcher
+type TopoWatcher struct {
+	topo   topo.Store
+	cancel context.CancelFunc
+	mu     sync.Mutex
+}
+
+// Start starts the topo store watcher
+func (w *TopoWatcher) Start(ch chan<- controller.ID) error {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	if w.cancel != nil {
+		return nil
+	}
+
+	eventCh := make(chan topoapi.Event, queueSize)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	err := w.topo.Watch(ctx, eventCh, nil)
+	if err != nil {
+		cancel()
+		return err
+	}
+	w.cancel = cancel
+
+	go func() {
+		for event := range eventCh {
+			log.Debugf("Received topo event '%s'", event.Object.ID)
+			if relation, ok := event.Object.Obj.(*topoapi.Object_Relation); ok &&
+				relation.Relation.KindID == topoapi.CONTROLS {
+				ch <- controller.NewID(relation.Relation.TgtEntityID)
+			}
+			if _, ok := event.Object.Obj.(*topoapi.Object_Entity); ok {
+				// If the entity object has configurable aspect then the controller
+				// can make a connection to it
+				err = event.Object.GetAspect(&topoapi.Configurable{})
+				if err == nil {
+					ch <- controller.NewID(event.Object.ID)
+				}
+			}
+
+		}
+		close(ch)
+	}()
+
+	return nil
+}
+
+// Stop stops the topology watcher
+func (w *TopoWatcher) Stop() {
+	w.mu.Lock()
+	if w.cancel != nil {
+		w.cancel()
+		w.cancel = nil
+	}
+	w.mu.Unlock()
+}
