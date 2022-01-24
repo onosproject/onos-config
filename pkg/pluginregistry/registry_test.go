1419c744e75749bdeeb6cf90a68a60fc7962dea3
Matteo <teone@users.noreply.github.com>
2022-01-24 12:25:14 -0800

    [SDRAN-1426] Fixing the Plugin Registry (#1424)
    * [SDRAN-1426] Fixing the Plugin Registry lookup method so that we can discover a plugin from a gNMI Set request
    
    * Using endpoint instead of port in the plugin registry, remove the Name attribute and use a temporary ID if the loading fails

---
+++ pkg/pluginregistry/registry_test.go
@@ -0,0 +1,90 @@
+// Copyright 2021-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pluginregistry
+
+import (
+	"context"
+	"fmt"
+	"github.com/onosproject/onos-api/go/onos/config/admin"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"strings"
+	"testing"
+)
+
+const testEndpoint1 = "testmodel1:5152"
+const testEndpoint2 = "testmodel2:5153"
+
+var pr = NewPluginRegistry(testEndpoint1, testEndpoint2)
+
+type MockModelPluginServiceClient struct {
+	getModelInfoResponse *admin.ModelInfoResponse
+}
+
+func (m MockModelPluginServiceClient) GetModelInfo(ctx context.Context, in *admin.ModelInfoRequest, opts ...grpc.CallOption) (*admin.ModelInfoResponse, error) {
+	return m.getModelInfoResponse, nil
+}
+
+func (MockModelPluginServiceClient) ValidateConfig(ctx context.Context, in *admin.ValidateConfigRequest, opts ...grpc.CallOption) (*admin.ValidateConfigResponse, error) {
+	return nil, nil
+}
+
+func (MockModelPluginServiceClient) GetPathValues(ctx context.Context, in *admin.PathValuesRequest, opts ...grpc.CallOption) (*admin.PathValuesResponse, error) {
+	return nil, nil
+}
+
+func TestLoadPluginInfo(t *testing.T) {
+
+	assert.Equal(t, 0, len(pr.plugins), "Plugins list is not empty at the beginning of the test")
+
+	plugin := &ModelPlugin{
+		Endpoint: "localhost:5152",
+	}
+
+	modelInfo := &admin.ModelInfo{Name: "Testmodel", Version: "1.0.0"}
+	mockClient := MockModelPluginServiceClient{
+		getModelInfoResponse: &admin.ModelInfoResponse{
+			ModelInfo: modelInfo,
+		},
+	}
+
+	pr.loadPluginInfo(mockClient, plugin)
+
+	assert.Equal(t, strings.ToLower(fmt.Sprintf("%s-%s", modelInfo.Name, modelInfo.Version)), plugin.ID, "Plugin ID is wrong")
+}
+
+func TestGetPlugin(t *testing.T) {
+
+	testID := "testmodel-1.0.0"
+	p1 := &ModelPlugin{
+		Endpoint: "localhost:5152",
+		ID:       testID,
+	}
+	p2 := &ModelPlugin{
+		Endpoint: "localhost:5153",
+		ID:       "someID",
+	}
+
+	pr.plugins[p1.ID] = p1
+	pr.plugins[p2.ID] = p2
+
+	plugin, found := pr.GetPlugin(testID)
+	assert.True(t, found, "Plugin not found")
+	assert.NotNil(t, plugin, "Plugin not found")
+
+	n, notFound := pr.GetPlugin("non-existing")
+	assert.False(t, notFound, "Plugin should not have been found")
+	assert.Nil(t, n)
+}
