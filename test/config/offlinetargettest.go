0fb92ff9177cdcb8d495f34750524510c33c45f4
Adib Rastegarnia <adib.rastegarnia@gmail.com>
2022-01-26 16:31:11 -0800

    Clean up some of the tests (#1442)

--- test/config/offlinetargettest.go
+++ test/config/offlinetargettest.go
@@ -16,9 +16,14 @@
 package config
 
 import (
-	"github.com/onosproject/onos-api/go/onos/topo"
 	"testing"
 	"time"
+
+	"github.com/onosproject/onos-test/pkg/onostest"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/onosproject/helmit/pkg/helm"
+	topoapi "github.com/onosproject/onos-api/go/onos/topo"
 
 	"github.com/onosproject/onos-config/test/utils/gnmi"
 	"github.com/onosproject/onos-config/test/utils/proto"
@@ -31,26 +36,33 @@ const (
 	offlineTargetName = "test-offline-device-1"
 )
 
-// TestOfflineDevice tests set/query of a single GNMI path to a single device that is initially not in the config
-func (s *TestSuite) TestOfflineDevice(t *testing.T) {
+// TestOfflineTarget tests set/query of a single GNMI path to a single target that is initially not in the config
+func (s *TestSuite) TestOfflineTarget(t *testing.T) {
 	// Make a GNMI client to use for requests
 	gnmiClient := gnmi.GetGNMIClientOrFail(t)
 
+	// create a target entity in topo
 	createOfflineTarget(t, offlineTargetName, "devicesim-1.0.x", "1.0.0", "")
 
-	devicePath := gnmi.GetTargetPathWithValue(offlineTargetName, modPath, modValue, proto.StringVal)
-	transactionID, transactionIndex := gnmi.SetGNMIValueOrFail(t, gnmiClient, devicePath, gnmi.NoPaths, []*gnmi_ext.Extension{})
+	// Sends a set request using onos-config NB
+	targetPath := gnmi.GetTargetPathWithValue(offlineTargetName, modPath, modValue, proto.StringVal)
+	gnmi.SetGNMIValueOrFail(t, gnmiClient, targetPath, gnmi.NoPaths, []*gnmi_ext.Extension{})
 
-	// Bring device online
-	simulator := gnmi.CreateSimulatorWithName(t, offlineTargetName)
-	defer gnmi.DeleteSimulator(t, simulator)
+	// Install simulator
+	simulator := helm.
+		Chart("device-simulator", onostest.OnosChartRepo).
+		Release(offlineTargetName).
+		Set("image.tag", "latest")
+	err := simulator.Install(true)
+	assert.NoError(t, err, "could not install device simulator %v", err)
 
-	// Wait for config to connect to the device
-	gnmi.WaitForTargetAvailable(t, topo.ID(simulator.Name()), time.Minute)
-	gnmi.CheckGNMIValue(t, gnmiClient, devicePath, modValue, 0, "Query after set returned the wrong value")
+	// Wait for config to connect to the target
+	gnmi.WaitForTargetAvailable(t, topoapi.ID(simulator.Name()), time.Minute)
+	gnmi.CheckGNMIValue(t, gnmiClient, targetPath, modValue, 0, "Query after set returned the wrong value")
 
-	// Check that the value was set properly on the device
-	gnmi.WaitForTransactionComplete(t, transactionID, transactionIndex, 10*time.Second)
-	deviceGnmiClient := gnmi.GetTargetGNMIClientOrFail(t, simulator)
-	gnmi.CheckTargetValue(t, deviceGnmiClient, devicePath, modValue)
+	// Check that the value was set properly on the target, wait for configuration gets completed
+	targetGnmiClient := gnmi.GetTargetGNMIClientOrFail(t, simulator)
+	gnmi.CheckTargetValue(t, targetGnmiClient, targetPath, modValue)
+	err = simulator.Uninstall()
+	assert.NoError(t, err)
 }
--- test/config/topotest.go
+++ test/config/topotest.go
@@ -16,155 +16,69 @@ package config
 
 import (
 	"context"
-	"github.com/onosproject/onos-api/go/onos/topo"
-	"github.com/onosproject/onos-config/test/utils/gnmi"
-	toposdk "github.com/onosproject/onos-ric-sdk-go/pkg/topo"
-	testutils "github.com/onosproject/onos-test/pkg/utils"
-	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
+
+	topoapi "github.com/onosproject/onos-api/go/onos/topo"
+	gnmiutils "github.com/onosproject/onos-config/test/utils/gnmi"
+	topoutils "github.com/onosproject/onos-config/test/utils/topo"
+	toposdk "github.com/onosproject/onos-ric-sdk-go/pkg/topo"
+	"github.com/stretchr/testify/assert"
 )
 
-// getEntityOrDie queries an entity of the given kind and fails if it isn't found
-func getEntityOrDie(ctx context.Context, t *testing.T, c toposdk.Client, kind string) topo.Object {
-	filter := &topo.Filters{
-		KindFilter: &topo.Filter{
-			Filter: &topo.Filter_Equal_{Equal_: &topo.EqualFilter{Value: kind}},
-		}}
-	entities, err := c.List(ctx, toposdk.WithListFilters(filter))
+// checkControlRelation queries a single control relation between the config and target, and fails if not found
+func (s *TestSuite) checkControlRelations(ctx context.Context, t *testing.T, targetID topoapi.ID, c toposdk.Client) []topoapi.Object {
+	numberOfConfigNodes := int(s.ConfigReplicaCount)
+	numberOfTargets := 1
+	numberOfControlRelationships := numberOfTargets * numberOfConfigNodes
+
+	relationObjects, err := c.List(ctx, toposdk.WithListFilters(topoutils.GetControlRelationFilter()))
 	assert.NoError(t, err)
-	assert.Len(t, entities, 1)
-	return entities[0]
-}
-
-// getOnosConfigEntityOrDie queries a single onos-config instance and fails if it isn't found
-func getOnosConfigEntityOrDie(ctx context.Context, t *testing.T, c toposdk.Client) topo.Object {
-	return getEntityOrDie(ctx, t, c, topo.ONOS_CONFIG)
-}
-
-// getDevicesimEntityOrDie queries a single device simulator instance and fails if it isn't found
-func getDevicesimEntityOrDie(ctx context.Context, t *testing.T, c toposdk.Client) topo.Object {
-	const (
-		devicesimKind = "devicesim-1.0.x"
-	)
-	return getEntityOrDie(ctx, t, c, devicesimKind)
-}
-
-// getRelationOrDie queries a single control relation between the config and target, and fails if not found
-func getRelationOrDie(ctx context.Context, t *testing.T, configID topo.ID, targetID topo.ID, c toposdk.Client) topo.Object {
-	filter := &topo.Filters{
-		RelationFilter: &topo.RelationFilter{
-			SrcId:        string(configID),
-			TargetId:     string(targetID),
-			Scope:        topo.RelationFilterScope_ALL,
-			RelationKind: topo.CONTROLS,
-		},
-	}
-
-	objs, err := c.List(ctx, toposdk.WithListFilters(filter))
-	assert.NoError(t, err)
-	assert.Len(t, objs, 3)
-
-	for _, obj := range objs {
-		if rel := obj.GetRelation(); rel != nil {
-			return obj
+	var filteredRelations []topoapi.Object
+	for _, relationObject := range relationObjects {
+		if relationObject.GetRelation().TgtEntityID == targetID {
+			filteredRelations = append(filteredRelations, relationObject)
 		}
 	}
-
-	assert.Fail(t, "Could not find relation")
-	return topo.Object{}
-}
-
-// waiForTopoEntries waits for the topo entries to be populated
-func waitForTopoEntriesOrDie(t *testing.T, err error, client toposdk.Client) {
-	var objs []topo.Object
-	maxWaitSeconds := 30
-	assert.True(t, testutils.Retry(maxWaitSeconds, time.Second,
-		func() bool {
-			objs, err = client.List(context.Background())
-			assert.NoError(t, err)
-			return len(objs) >= 3
-		}))
-	assert.NoError(t, err)
+	assert.Equal(t, numberOfControlRelationships, len(filteredRelations))
+	return filteredRelations
 }
 
 // checkTopo makes sure that the topo entries are correct
-func checkTopo(t *testing.T) {
-	var (
-		configEntity *topo.Entity
-		configObject topo.Object
-		configID     topo.ID
-
-		targetEntity *topo.Entity
-		targetObject topo.Object
-		targetID     topo.ID
-
-		relation       *topo.Relation
-		relationObject topo.Object
-	)
+func (s *TestSuite) checkTopo(t *testing.T, targetID topoapi.ID) {
 
 	// Get a topology API client
-	client, err := gnmi.NewTopoClient()
+	client, err := gnmiutils.NewTopoClient()
 	assert.NoError(t, err)
 	assert.NotNil(t, client)
 
-	// Wait for the topo entries to be set up
-	waitForTopoEntriesOrDie(t, err, client)
-
-	// Find the entity for onos-config
-	configObject = getOnosConfigEntityOrDie(context.Background(), t, client)
-	configID = configObject.ID
-	configEntity = configObject.GetEntity()
-
-	// Find the entity for the simulated target
-	targetObject = getDevicesimEntityOrDie(context.Background(), t, client)
-	targetID = targetObject.ID
-	targetEntity = targetObject.GetEntity()
-
-	// Find the control relation from onos-config to the simulated target
-	relationObject = getRelationOrDie(context.Background(), t, configID, targetID, client)
-	relation = relationObject.GetRelation()
-
-	// Check that we found a controls relation
-	assert.NotNil(t, relation)
-	assert.Equal(t, topo.ID(topo.CONTROLS), relation.KindID)
-
-	// Check that the source and target IDs are correct
-	assert.Len(t, configEntity.SrcRelationIDs, 1)
-	assert.Len(t, targetEntity.TgtRelationIDs, 1)
-
-	configRelationID := configEntity.SrcRelationIDs[0]
-	targetRelationID := targetEntity.TgtRelationIDs[0]
-
-	assert.Equal(t, configRelationID, targetRelationID)
-	assert.Equal(t, configID, relation.SrcEntityID)
-	assert.Equal(t, targetID, relation.TgtEntityID)
-
-	// Check onos-config aspects
-	assert.Len(t, configObject.Aspects, 1)
-	lease := &topo.Lease{}
-	err = configObject.GetAspect(lease)
-	assert.NoError(t, err)
-
-	// Check the target simulator aspects
-	assert.Greater(t, len(targetObject.Aspects), 1)
-	configurable := &topo.Configurable{}
-	err = targetObject.GetAspect(configurable)
-	assert.NoError(t, err)
-
-	mastershipState := &topo.MastershipState{}
-	err = targetObject.GetAspect(mastershipState)
-	assert.NoError(t, err)
-	assert.Equal(t, relationObject.ID, topo.ID(mastershipState.NodeId))
+	// check number of control relations from onos-config instances to the simulated target
+	relationObjects := s.checkControlRelations(context.Background(), t, targetID, client)
+	for _, relationObject := range relationObjects {
+		controlRelation := relationObject.GetRelation()
+		onosConfigNode, err := client.Get(context.Background(), controlRelation.SrcEntityID)
+		assert.NoError(t, err)
+		leaseAspect := &topoapi.Lease{}
+		err = onosConfigNode.GetAspect(leaseAspect)
+		assert.NoError(t, err)
+		// Find the entity for the simulated target
+		targetObject, err := client.Get(context.Background(), controlRelation.TgtEntityID)
+		assert.NoError(t, err)
+		// Check the target simulator aspects
+		configurable := &topoapi.Configurable{}
+		err = targetObject.GetAspect(configurable)
+		assert.NoError(t, err)
+	}
 }
 
 // TestTopoIntegration checks that the correct topology entities and relations are created
 func (s *TestSuite) TestTopoIntegration(t *testing.T) {
-	// Create a simulated device
-	simulator := gnmi.CreateSimulator(t)
-	defer gnmi.DeleteSimulator(t, simulator)
-
+	// Create simulated targets
+	targetID := "test-topo-integration-target-1"
+	simulator := gnmiutils.CreateSimulatorWithName(t, targetID)
 	assert.NotNil(t, simulator)
+	gnmiutils.WaitForTargetAvailable(t, topoapi.ID(targetID), 2*time.Minute)
+	defer gnmiutils.DeleteSimulator(t, simulator)
+	s.checkTopo(t, topoapi.ID(targetID))
 
-	checkTopo(t)
 }
