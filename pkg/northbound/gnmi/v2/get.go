1e7a0ff34c3b5ed79e9d37599d45f62a511eddd1
Ray Milkey <ray@opennetworking.org>
2022-01-28 16:59:44 -0800

    Fix RBAC tests (#1460)

--- pkg/northbound/gnmi/v2/get.go
+++ pkg/northbound/gnmi/v2/get.go
@@ -18,10 +18,12 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	topoapi "github.com/onosproject/onos-api/go/onos/topo"
 	"github.com/onosproject/onos-config/pkg/store/configuration"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 
@@ -46,7 +48,7 @@ func (s *Server) Get(ctx context.Context, req *gnmi.GetRequest) (*gnmi.GetRespon
 	groups := make([]string, 0)
 	if md := metautils.ExtractIncoming(ctx); md != nil && md.Get("name") != "" {
 		groups = append(groups, strings.Split(md.Get("groups"), ";")...)
-		log.Infof("gNMI Get() called by '%s (%s)'. Groups %v. Token %s",
+		log.Debugf("gNMI Get() called by '%s (%s)'. Groups %v. Token %s",
 			md.Get("name"), md.Get("email"), groups, md.Get("at_hash"))
 	}
 	if req == nil || (req.GetEncoding() != gnmi.Encoding_PROTO && req.GetEncoding() != gnmi.Encoding_JSON_IETF && req.GetEncoding() != gnmi.Encoding_JSON) {
@@ -139,7 +141,21 @@ func (s *Server) getUpdate(ctx context.Context, targetInfo targetInfo, prefix *g
 	return createUpdate(prefix, path, configValuesAllowed, encoding)
 }
 
+func filterTargetForURL(target string) string {
+	re := regexp.MustCompile(`[.-]`)
+	return re.ReplaceAllString(target, "_")
+}
+
 func (s *Server) checkOpaAllowed(ctx context.Context, targetInfo targetInfo, configValues []*configapi.PathValue, groups []string) ([]*configapi.PathValue, error) {
+	modelPlugin, err := s.getModelPlugin(ctx, topoapi.ID(targetInfo.targetID))
+	if err != nil {
+		log.Warn(err)
+		return nil, err
+	}
+
+	targetVersion := filterTargetForURL(modelPlugin.Info.Version)
+	targetType := filterTargetForURL(modelPlugin.Info.Name)
+
 	jsonTree, err := tree.BuildTree(configValues, true)
 	if err != nil {
 		return nil, err
@@ -147,10 +163,13 @@ func (s *Server) checkOpaAllowed(ctx context.Context, targetInfo targetInfo, con
 	// add 'input' and `groups` objects to the JSON
 	jsonTreeInput := utils.FormatInput(jsonTree, groups)
 	log.Debugf("OPA Input:\n%s", jsonTreeInput)
+
 	client := &http.Client{}
 	// POST to OPA sidecar
 	opaURL := fmt.Sprintf("http://localhost:%d/v1/data/%s_%s/allowed?pretty=%v&metrics=%v", 8181,
-		strings.ToLower(string(targetInfo.targetType)), strings.ReplaceAll(string(targetInfo.targetVersion), ".", "_"), false, true)
+		targetType, targetVersion, false, true)
+
+	log.Debugf("OPA URL is %s", opaURL)
 	resp, err := client.Post(opaURL, "application/json", bytes.NewBuffer([]byte(jsonTreeInput)))
 	if err != nil {
 		log.Warnf("Error sending request to OPA sidecar %s %s", opaURL, err.Error())
@@ -170,12 +189,6 @@ func (s *Server) checkOpaAllowed(ctx context.Context, targetInfo targetInfo, con
 		return nil, nil
 	}
 
-	modelName := utils.ToModelNameV2(targetInfo.targetType, targetInfo.targetVersion)
-	modelPlugin, ok := s.pluginRegistry.GetPlugin(modelName)
-	if !ok {
-		return nil, errors.NewNotFound("model plugin %s not found", modelPlugin.ID)
-	}
-
+	log.Debugf("body text of respose from OPA:\n%s", bodyText)
 	return modelPlugin.GetPathValues(ctx, "", []byte(bodyText))
-
 }
