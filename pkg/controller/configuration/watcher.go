0cdc199262b4c588114fd89a9c62e0d65279c849
Jordan Halterman <jordan.halterman@gmail.com>
2022-01-28 16:08:10 -0800

    Remove relations from configuration controller watchers

--- pkg/controller/configuration/watcher.go
+++ pkg/controller/configuration/watcher.go
@@ -103,11 +103,6 @@ func (w *TopoWatcher) Start(ch chan<- controller.ID) error {
 					// TODO: Get target type and version from topo entity
 					ch <- controller.NewID(configuration.NewID(configapi.TargetID(event.Object.ID), "", ""))
 				}
-			} else if relation, ok := event.Object.Obj.(*topoapi.Object_Relation); ok {
-				if relation.Relation.KindID == topoapi.CONTROLS {
-					// TODO: Get target type and version from topo entity
-					ch <- controller.NewID(configuration.NewID(configapi.TargetID(relation.Relation.TgtEntityID), "", ""))
-				}
 			}
 		}
 	}()
