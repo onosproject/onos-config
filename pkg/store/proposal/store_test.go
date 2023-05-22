5475785a00c000d90515d324340cb7c8a57c9107
Jordan Halterman <jordan.halterman@gmail.com>
2023-05-22 16:47:25 -0700

    Refactor proposal store to use per-target logs

--- pkg/store/proposal/store_test.go
+++ pkg/store/proposal/store_test.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	configapi "github.com/onosproject/onos-api/go/onos/config/v2"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -24,159 +25,204 @@ func TestProposalStore(t *testing.T) {
 	store2, err := NewAtomixStore(cluster)
 	assert.NoError(t, err)
 
-	target1 := configapi.TargetID("target-1")
-	target2 := configapi.TargetID("target-2")
-
-	ch := make(chan configapi.ProposalEvent)
-	err = store2.Watch(context.Background(), ch)
+	eventCh := make(chan configapi.ProposalEvent)
+	err = store2.Watch(context.Background(), eventCh)
 	assert.NoError(t, err)
 
-	target1ConfigValues := make(map[string]*configapi.PathValue)
-	target1ConfigValues["/foo"] = &configapi.PathValue{
-		Value: configapi.TypedValue{
-			Bytes: []byte("Hello world!"),
-			Type:  configapi.ValueType_STRING,
-		},
+	target := configapi.Target{
+		ID:      "target-1",
+		Type:    "foo",
+		Version: "1",
 	}
 
-	target1Config := &configapi.Proposal{
-		ID:               configapi.ProposalID(target1),
-		TargetID:         target1,
-		TransactionIndex: 1,
-		Details: &configapi.Proposal_Change{
-			Change: &configapi.ChangeProposal{
-				Values: target1ConfigValues,
+	proposal1 := &configapi.Proposal{
+		ObjectMeta: configapi.ObjectMeta{
+			Key: "proposal-1",
+		},
+		ID: configapi.ProposalID{
+			Target: target,
+		},
+		Values: map[string]*configapi.PathValue{
+			"foo": {
+				Value: configapi.TypedValue{
+					Bytes: []byte("Hello world!"),
+					Type:  configapi.ValueType_STRING,
+				},
+			},
+			"bar": {
+				Value: configapi.TypedValue{
+					Bytes: []byte("Hello world again!"),
+					Type:  configapi.ValueType_STRING,
+				},
 			},
 		},
 	}
 
-	target2ConfigValues := make(map[string]*configapi.PathValue)
-	target2ConfigValues["/foo"] = &configapi.PathValue{
-		Value: configapi.TypedValue{
-			Bytes: []byte("Hello world again!"),
-			Type:  configapi.ValueType_STRING,
+	proposal2 := &configapi.Proposal{
+		ObjectMeta: configapi.ObjectMeta{
+			Key: "proposal-2",
 		},
-	}
-	target2Config := &configapi.Proposal{
-		ID:               configapi.ProposalID(target2),
-		TargetID:         target2,
-		TransactionIndex: 1,
-		Details: &configapi.Proposal_Change{
-			Change: &configapi.ChangeProposal{
-				Values: target2ConfigValues,
+		ID: configapi.ProposalID{
+			Target: target,
+		},
+		Values: map[string]*configapi.PathValue{
+			"foo": {
+				Deleted: true,
 			},
 		},
 	}
 
-	err = store1.Create(context.TODO(), target1Config)
+	// Create a new proposal
+	err = store1.Create(context.TODO(), proposal1)
 	assert.NoError(t, err)
-	assert.Equal(t, configapi.ProposalID(target1), target1Config.ID)
-	assert.NotEqual(t, configapi.Revision(0), target1Config.Revision)
-
-	err = store2.Create(context.TODO(), target2Config)
-	assert.NoError(t, err)
-	assert.Equal(t, configapi.ProposalID(target2), target2Config.ID)
-	assert.NotEqual(t, configapi.Revision(0), target2Config.Revision)
+	assert.Equal(t, "proposal-1", proposal1.Key)
+	assert.Equal(t, configapi.Index(1), proposal1.ID.Index)
+	assert.NotEqual(t, configapi.Revision(0), proposal1.Revision)
 
 	// Get the proposal
-	target1Config, err = store2.Get(context.TODO(), configapi.ProposalID(target1))
+	proposal1, err = store2.GetKey(context.TODO(), target, "proposal-1")
 	assert.NoError(t, err)
-	assert.NotNil(t, target1Config)
-	assert.Equal(t, configapi.ProposalID(target1), target1Config.ID)
-	assert.NotEqual(t, configapi.Revision(0), target1Config.Revision)
+	assert.NotNil(t, proposal1)
+	assert.Equal(t, "proposal-1", proposal1.Key)
+	assert.Equal(t, configapi.Index(1), proposal1.ID.Index)
+	assert.NotEqual(t, configapi.Revision(0), proposal1.Revision)
 
-	// Verify events were received for the proposals
-	proposalEvent := nextEvent(t, ch)
-	assert.NotNil(t, proposalEvent)
-	proposalEvent = nextEvent(t, ch)
-	assert.NotNil(t, proposalEvent)
+	proposalEvent := nextProposal(t, eventCh)
+	assert.Equal(t, "proposal-1", proposalEvent.Key)
+
+	// Create another proposal
+	err = store2.Create(context.TODO(), proposal2)
+	assert.NoError(t, err)
+	assert.Equal(t, "proposal-2", proposal2.Key)
+	assert.Equal(t, configapi.Index(2), proposal2.ID.Index)
+	assert.NotEqual(t, configapi.Revision(0), proposal2.Revision)
+
+	proposalEvent = nextProposal(t, eventCh)
+	assert.Equal(t, "proposal-2", proposalEvent.Key)
 
 	// Watch events for a specific proposal
 	proposalCh := make(chan configapi.ProposalEvent)
-	err = store1.Watch(context.TODO(), proposalCh, WithProposalID(target2Config.ID))
+	err = store1.Watch(context.TODO(), proposalCh, WithProposalID(proposal2.ID))
 	assert.NoError(t, err)
 
 	// Update one of the proposals
-	revision := target2Config.Revision
-	err = store1.Update(context.TODO(), target2Config)
+	revision := proposal2.Revision
+	err = store1.Update(context.TODO(), proposal2)
 	assert.NoError(t, err)
-	assert.NotEqual(t, revision, target2Config.Revision)
+	assert.NotEqual(t, revision, proposal2.Revision)
 
-	event := <-proposalCh
-	assert.Equal(t, target2Config.ID, event.Proposal.ID)
+	proposalEvent = nextProposal(t, eventCh)
+	assert.Equal(t, "proposal-2", proposalEvent.Key)
 
-	// Lists proposals
-	proposalList, err := store1.List(context.TODO())
-	assert.NoError(t, err)
-	assert.Equal(t, 2, len(proposalList))
+	event := nextEvent(t, proposalCh)
+	assert.Equal(t, proposal2.ID, event.Proposal.ID)
 
 	// Read and then update the proposal
-	target2Config, err = store2.Get(context.TODO(), configapi.ProposalID(target2))
+	proposal2, err = store2.GetKey(context.TODO(), target, "proposal-2")
 	assert.NoError(t, err)
-	assert.NotNil(t, target2Config)
+	assert.NotNil(t, proposal2)
 	now := time.Now()
-	target2Config.Status.Phases.Initialize = &configapi.ProposalInitializePhase{
-		ProposalPhaseStatus: configapi.ProposalPhaseStatus{
-			Start: &now,
-		},
-	}
-	revision = target2Config.Revision
-	err = store1.Update(context.TODO(), target2Config)
+	proposal2.Status.Change = &configapi.ProposalChangeStatus{}
+	proposal2.Status.Change.Commit.Start = &now
+	revision = proposal2.Revision
+	err = store1.Update(context.TODO(), proposal2)
 	assert.NoError(t, err)
-	assert.NotEqual(t, revision, target2Config.Revision)
+	assert.NotEqual(t, revision, proposal2.Revision)
 
-	event = <-proposalCh
-	assert.Equal(t, target2Config.ID, event.Proposal.ID)
+	proposalEvent = nextProposal(t, eventCh)
+	assert.Equal(t, "proposal-2", proposalEvent.Key)
+
+	event = nextEvent(t, proposalCh)
+	assert.Equal(t, proposal2.ID, event.Proposal.ID)
 
 	// Verify that concurrent updates fail
-	target1Config11, err := store1.Get(context.TODO(), configapi.ProposalID(target1))
+	proposal11, err := store1.GetKey(context.TODO(), target, "proposal-1")
 	assert.NoError(t, err)
-	target1Config12, err := store2.Get(context.TODO(), configapi.ProposalID(target1))
-	assert.NoError(t, err)
-
-	target1Config11.Status.Phases.Initialize = &configapi.ProposalInitializePhase{
-		ProposalPhaseStatus: configapi.ProposalPhaseStatus{
-			Start: &now,
-		},
-	}
-	err = store1.Update(context.TODO(), target1Config11)
+	proposal12, err := store2.GetKey(context.TODO(), target, "proposal-1")
 	assert.NoError(t, err)
 
-	target1Config12.Status.Phases.Initialize = &configapi.ProposalInitializePhase{
-		ProposalPhaseStatus: configapi.ProposalPhaseStatus{
-			Start: &now,
-		},
-	}
-	err = store2.Update(context.TODO(), target1Config12)
+	proposal11.Status.Change = &configapi.ProposalChangeStatus{}
+	proposal11.Status.Change.Commit.Start = &now
+	err = store1.Update(context.TODO(), proposal11)
+	assert.NoError(t, err)
+
+	proposal11.Status.Change = &configapi.ProposalChangeStatus{}
+	proposal11.Status.Change.Commit.Start = &now
+	err = store2.Update(context.TODO(), proposal12)
 	assert.Error(t, err)
 
-	// Verify events were received again
-	proposalEvent = nextEvent(t, ch)
-	assert.NotNil(t, proposalEvent)
-	proposalEvent = nextEvent(t, ch)
-	assert.NotNil(t, proposalEvent)
-	proposalEvent = nextEvent(t, ch)
-	assert.NotNil(t, proposalEvent)
+	proposalEvent = nextProposal(t, eventCh)
+	assert.Equal(t, "proposal-1", proposalEvent.Key)
 
-	// Checks list of proposal after deleting a proposal
-	proposalList, err = store2.List(context.TODO())
+	// List the proposals
+	proposals, err := store1.List(context.TODO())
 	assert.NoError(t, err)
-	assert.Equal(t, 2, len(proposalList))
+	assert.Equal(t, 2, len(proposals))
 
-	err = store1.Close(context.TODO())
+	proposal := &configapi.Proposal{
+		ObjectMeta: configapi.ObjectMeta{
+			Key: "proposal-3",
+		},
+		ID: configapi.ProposalID{
+			Target: target,
+		},
+		Values: map[string]*configapi.PathValue{
+			"foo": {
+				Value: configapi.TypedValue{
+					Bytes: []byte("Hello world!"),
+					Type:  configapi.ValueType_STRING,
+				},
+			},
+		},
+	}
+
+	err = store1.Create(context.TODO(), proposal)
 	assert.NoError(t, err)
 
-	err = store2.Close(context.TODO())
+	proposal = &configapi.Proposal{
+		ObjectMeta: configapi.ObjectMeta{
+			Key: "proposal-4",
+		},
+		ID: configapi.ProposalID{
+			Target: target,
+		},
+		Values: map[string]*configapi.PathValue{
+			"bar": {
+				Value: configapi.TypedValue{
+					Bytes: []byte("Hello world!"),
+					Type:  configapi.ValueType_STRING,
+				},
+			},
+		},
+	}
+
+	err = store1.Create(context.TODO(), proposal)
 	assert.NoError(t, err)
 
+	eventCh = make(chan configapi.ProposalEvent)
+	err = store1.Watch(context.TODO(), eventCh, WithReplay())
+	assert.NoError(t, err)
+
+	proposal = nextProposal(t, eventCh)
+	assert.Equal(t, configapi.Index(1), proposal.ID.Index)
+	proposal = nextProposal(t, eventCh)
+	assert.Equal(t, configapi.Index(2), proposal.ID.Index)
+	proposal = nextProposal(t, eventCh)
+	assert.Equal(t, configapi.Index(3), proposal.ID.Index)
+	proposal = nextProposal(t, eventCh)
+	assert.Equal(t, configapi.Index(4), proposal.ID.Index)
 }
 
-func nextEvent(t *testing.T, ch chan configapi.ProposalEvent) *configapi.Proposal {
+func nextEvent(t *testing.T, ch chan configapi.ProposalEvent) *configapi.ProposalEvent {
 	select {
-	case c := <-ch:
-		return &c.Proposal
+	case e := <-ch:
+		return &e
 	case <-time.After(5 * time.Second):
 		t.FailNow()
 	}
 	return nil
+}
+
+func nextProposal(t *testing.T, ch chan configapi.ProposalEvent) *configapi.Proposal {
+	return &nextEvent(t, ch).Proposal
 }
